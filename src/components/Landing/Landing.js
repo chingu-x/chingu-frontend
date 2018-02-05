eb0fa2f89a12f17947b334bba359ae2e877133ce
Vannya <van.tabbert@gmail.com>
2018-02-05 15:52:06 -0600

    upper landing complete

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,15 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import earth from "../../styles/assets/Global Image-02.png";
 import "./Landing.css";
 
 class Landing extends Component {
-  state = {  }
+  state = {};
   render() {
     return (
       <div className="landing">
-        Landing
+      <img className="landing-img" src={earth} alt="" />
+        <div className="tagline-box">
+          <div className="tagline">Code More</div>
+          <div className="tagline">Learn More</div>
+          <div className="tagline">Build More</div>
+          <Link to="/signup"><button className="big-green-btn">Apply</button></Link>
+        </div>
       </div>
     );
   }
 }
 
-export default Landing; 
+export default Landing;
