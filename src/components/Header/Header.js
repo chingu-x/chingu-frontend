6e1eb8f1537cd9dfb837997ec9ea5d8c1cdd3a90
Vannya <van.tabbert@gmail.com>
2018-02-05 14:27:36 -0600

    update header classes for landing page styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -16,25 +16,26 @@ class Header extends Component {
 
   renderRightNav() {
     let loggedIn = window.localStorage.token;
-    let { user, error } = this.props.data;
+    let { error } = this.props.data;
     if (error) {
-      
+      //do nothing.  this will throw an error when not logged in.  apollo client known issue.
+      //this if statement will clear it out of the console for now.
     };
     if (!!loggedIn) {
       return [
-        <Link key="1" className="btn" to="/" onClick={e => this.handleLogout(e)}>
+        <Link key="1" className={window.location.pathname === "/" ? "btn btn-light" : "btn"} to="/" onClick={e => this.handleLogout(e)}>
           Log Out
         </Link>,
-        <Link key="2" className="btn" to="/" >
+        <Link key="2" className={window.location.pathname === "/" ? "btn btn-light" : "btn"} to="/" >
           <i className="far fa-user fa-2x" />
         </Link>
       ];
     } else {
       return [
-        <Link key="1" className="btn" to="/signup">
+        <Link key="1" className={window.location.pathname === "/" ? "btn btn-light" : "btn"} to="/signup">
           Sign Up
         </Link>,
-        <Link key="2" className="btn" to="/login">
+        <Link key="2" className={window.location.pathname === "/" ? "btn btn-light" : "btn"} to="/login">
           Log In
         </Link>
       ];
@@ -43,10 +44,10 @@ class Header extends Component {
 
   render() {
     return (
-      <div className="header">
+      <div className={window.location.pathname === "/" ? "header header-dark" : "header"}>
         <div className="header-left">
           <div className="nav-logo">
-            <Link to="/">CHINGU</Link>
+            <Link className={window.location.pathname === "/" ? "nav-light" : ""} to="/">CHINGU</Link>
           </div>
         </div>
         <div className="header-right">{this.renderRightNav()}</div>
