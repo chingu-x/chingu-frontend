150bca31d0222363362c11b887cecd0cf000977e
The-Vampiire <vampiirecodes@gmail.com>
2018-07-17 16:46:38 -0400

    refactored to use latest Apollo Boost client

--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { ApolloProvider } from "react-apollo";
-import { ApolloClient } from "apollo-client";
-import { ApolloLink } from "apollo-link";
-import { createHttpLink } from "apollo-link-http";
-import { InMemoryCache } from "apollo-cache-inmemory";
+import ApolloClient from 'apollo-boost';
 import { createStore, applyMiddleware } from "redux";
-import {Provider} from "react-redux"
+import { Provider } from "react-redux"
 import App from "./App";
 import ScrollToTop from "./ScrollToTop";
 import "./styles/fontawesome/webfonts/fontawesome-all.css";
@@ -17,33 +14,34 @@ import "./styles/main.css";
 import reducer from "./reducers";
 import registerServiceWorker from "./registerServiceWorker";
 
+// TODO: lets refactor this to get the appropriate URI from an environment var?
 let API_URI;
 if (window.location.host.indexOf("chingu-staging") > -1 || window.location.host.indexOf("localhost") > -1) {
-  API_URI = "https://chingu-api-dev.herokuapp.com/graphql";
+  // API_URI = "https://chingu-api-dev.herokuapp.com/graphql"; // TODO: update domain
+  API_URI = "http://localhost:8008/graphql" // TODO: temporary - REMOVE
 } else {
-  API_URI = "https://chingu-api.herokuapp.com/graphql";
+  API_URI = "https://chingu-api.herokuapp.com/graphql"; // TODO: update domain
 }
+ 
+const token = localStorage.getItem("token"); // auth token
 
-const httpLink = createHttpLink({ uri: API_URI });
-
-const middlewareAuth = new ApolloLink((operation, forward) => {
-  const token = localStorage.getItem("token");
-  const authorizationHeader = token ? `${token}` : null;
-  operation.setContext({
-    headers: {
-      authorization: authorizationHeader
-    }
-  });
-  return forward(operation);
-});
-
+// configure client
+  // sets API endpoint
+  // sets auth headers to be passed along every request
+  // MemCache is built in already
 const client = new ApolloClient({
-  link: middlewareAuth.concat(httpLink),
-  cache: new InMemoryCache()
+  uri: API_URI,
+  request: operation => operation.setContext({
+    headers: {
+      authorization: token ? `Bearer ${token}` : null,
+    }
+  })
 });
 
 const store = createStore(reducer, {}, applyMiddleware());
 
+// ApolloProvider wraps the root component and provides ApolloClient features
+// to all child components. Similar to how the redux Provider does the same for state
 ReactDOM.render(
   <Provider store={store}>
     <ApolloProvider client={client}>
@@ -56,4 +54,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
+
 registerServiceWorker();
