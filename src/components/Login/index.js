173d0335f3cb5f3ed95c4abc7a481f2205449d1c
serpient <littlegangwolf@gmail.com>
2018-07-19 12:37:06 -0700

    login modal styling

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,17 +1,19 @@
 import React from "react";
-
+import './Login.css';
 // TODO: add state generator
 //   generate state and store in local storage
 //   on redirect confirm state match and remove
 
 const Login = () => (
-  <div className="login-box">
-    <div className="login-title">Github Authentication</div>
-    <a
-      href={`https://github.com/login/oauth/authorize?client_id=e015fd9cc874fa5a34bf`}
-    >
-      <button className="github-auth"><img alt="github-icon" src={'https://i.imgur.com/UBZgVgQ.png'} />Log in with Github</button>
-    </a>
+  <div className="login-container">
+    <div className="login-box">
+      <div className="login-title">Github Authentication</div>
+      <a
+        href={`https://github.com/login/oauth/authorize?client_id=e015fd9cc874fa5a34bf`}
+      >
+        <button className="github-auth"><img alt="github-icon" className="github-icon" src={'https://i.imgur.com/UBZgVgQ.png'} />Log in with Github</button>
+      </a>
+    </div>
   </div>
 );
 
