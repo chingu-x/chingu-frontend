684f9b82175e32be7f8d054e32b51a5ef35c63e3
serpient <littlegangwolf@gmail.com>
2018-07-19 23:21:33 -0700

    cleaned up apollo client setup in index.js, removed old code and redux

--- src/index.js
+++ src/index.js
@@ -2,35 +2,25 @@ import 'babel-polyfill';
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+
 import { ApolloProvider } from "react-apollo";
 import ApolloClient from 'apollo-boost';
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux"
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
 import App from "./App";
 import ScrollToTop from "./ScrollToTop";
 import "./styles/fontawesome/webfonts/fontawesome-all.css";
 import "./styles/main.css";
 
-import reducer from "./reducers";
 import registerServiceWorker from "./registerServiceWorker";
-
-// TODO: lets refactor this to get the appropriate URI from an environment var?
-let API_URI;
-if (window.location.host.indexOf("chingu-staging") > -1 || window.location.host.indexOf("localhost") > -1) {
-  // API_URI = "https://chingu-api-dev.herokuapp.com/graphql"; // TODO: update domain
-  API_URI = "http://localhost:8008/graphql" // TODO: temporary - REMOVE
-} else {
-  API_URI = "https://api.chingu.io/graphql"; // TODO: update domain
-}
  
 const token = localStorage.getItem("token"); // auth token
+const httpLink = new HttpLink({ uri: 'https://api.chingu.io/graphql' });
 
-// configure client
-  // sets API endpoint
-  // sets auth headers to be passed along every request
-  // MemCache is built in already
 const client = new ApolloClient({
-  uri: API_URI,
+  uri: httpLink,
+  cache: new InMemoryCache(),
   request: operation => operation.setContext({
     headers: {
       authorization: token ? `Bearer ${token}` : null,
@@ -38,20 +28,16 @@ const client = new ApolloClient({
   })
 });
 
-const store = createStore(reducer, {}, applyMiddleware());
-
 // ApolloProvider wraps the root component and provides ApolloClient features
 // to all child components. Similar to how the redux Provider does the same for state
 ReactDOM.render(
-  <Provider store={store}>
     <ApolloProvider client={client}>
       <BrowserRouter>
         <ScrollToTop>
           <App />
         </ScrollToTop>
       </BrowserRouter>
-    </ApolloProvider>
-  </Provider>,
+    </ApolloProvider>,
   document.getElementById("root")
 );
 
