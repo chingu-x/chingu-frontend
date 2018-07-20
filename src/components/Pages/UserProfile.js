a4a38cd1fd90be5e8fb8eba29dc1ab76f2f37b29
Tony Luo <luo.tony37@gmail.com>
2018-07-20 16:22:47 -0700

    Add user profile page and styles

---
+++ src/components/Pages/UserProfile.js
@@ -0,0 +1,35 @@
+import * as React from "react";
+import * as Cards from "../VoyageCard/VoyageCard";
+import UserSideBar from "../UserProfile/UserProfile";
+/**
+ * TODO:
+ * 1. put user-voyages in own section
+ **/
+
+class UserProfile extends React.Component {
+  render() {
+    return (
+      <div className="user-profile-container">
+        <aside className="user-profile">
+          <UserSideBar />
+        </aside>
+        <main className="user-voyages">
+          <section className="user-voyage">
+            <p>Current Voyages</p>
+            <Cards.CurrentVoyageCard />
+          </section>
+          <section className="user-voyage">
+            <p>Past Voyages</p>
+            <div>
+              <Cards.CurrentVoyageCardWithTeam />
+              <Cards.PreviousVoyageCardWithTeam />
+              <Cards.UpcomingVoyageCard />
+            </div>
+          </section>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default UserProfile;
