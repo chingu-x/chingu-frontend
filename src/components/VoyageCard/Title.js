f42167639a7a66ba0e5415e8f8eabf864e8f4142
Tony Luo <luo.tony37@gmail.com>
2018-07-20 16:21:42 -0700

    Add voyage card components and styles

---
+++ src/components/VoyageCard/Title.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+/**
+ * TODO:
+ * 1. allow title to take in arguments
+ **/
+
+const Title = () => {
+  return <p>Turtles-Team-11</p>;
+};
+
+export default Title;
---
+++ src/components/VoyageCard/WeeklyCheckIn.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+/**
+ * TODO:
+ * 1. add way to navigate to check in page
+ **/
+
+const WeeklyCheckIn = () => {
+  return (
+    <div className="weekly-checkin-container">
+      <button>Weekly Check-In</button>
+    </div>
+  );
+};
+
+export default WeeklyCheckIn;
