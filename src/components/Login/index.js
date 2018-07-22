d93c6b0eb0d781c61c8bc94af0b9bae7105c680a
thinktwice13 <marioo.krajacic@gmail.com>
2018-07-22 14:38:33 +0200

    Fix Github login button underline

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -9,9 +9,15 @@ const Login = () => (
     <div className="login-box">
       <div className="login-title">Github Authentication</div>
       <a
+        className="github-auth"
         href={`https://github.com/login/oauth/authorize?client_id=e015fd9cc874fa5a34bf`}
       >
-        <button className="github-auth"><img alt="github-icon" className="github-icon" src={'https://i.imgur.com/UBZgVgQ.png'} />Log in with Github</button>
+        <img 
+          className="github-icon" 
+          src={'https://i.imgur.com/UBZgVgQ.png'} 
+          alt="github-icon" 
+        />
+        Log in with Github
       </a>
     </div>
   </div>
