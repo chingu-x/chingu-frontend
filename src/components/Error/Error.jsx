254f53f0ea3ff65d8b242a3b3ea2000f55d03a23
serpient <littlegangwolf@gmail.com>
2018-07-23 16:45:37 -0700

    alt alts to image

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -5,7 +5,7 @@ class Error extends React.Component {
     return (
       <div className="error-page-container">
         <div className="error-page-modal">
-          <img className="error-img" src={require('../../assets/error.png')} />
+          <img alt="error" className="error-img" src={require('../../assets/error.png')} />
           <div className="error-message">{this.props.error}</div>
           <hr className="error-hline" />
           <button className="error-go-back-btn">
--- src/components/Success/Success.jsx
+++ src/components/Success/Success.jsx
@@ -5,7 +5,7 @@ class SuccessForm extends React.Component {
     render() {
         return (
             <div className="form-success">
-                  <img className="form-success-img" src={require('../../assets/success.png')} />
+                  <img alt="success" className="form-success-img" src={require('../../assets/success.png')} />
                   <br />
                   Thank you!
                   <hr className="form-hline"/>
