81fb6e877fdb039da32738bf6c4cd9403f97e399
thinktwice13 <marioo.krajacic@gmail.com>
2018-07-23 02:40:35 +0200

    Cleanup Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,97 +1,13 @@
-// import * as React from 'react';
-// import { Link } from "react-router-dom";
-// import { graphql } from "react-apollo";
-
-// import currentUserQuery from "../../queries/currentUserQuery";
-
-// class Header extends React.Component {
-//   state = {};
-
-  // handleLogout(e){
-  //   e.preventDefault();
-  //   window.localStorage.removeItem("token");
-  //   window.location = "/";
-  // };
-
-//   // Right nav link link to Dash is disabled while image structure being set up by backend team.
-//   // Icon will need to be redirected to /myaccount once complete.
-//   renderRightNav() {
-//     let loggedIn = window.localStorage.token, loginMenuItems;
-//     let { error } = this.props.data;
-
-//     let menu = [
-//       <a key="1" href="https://medium.com/chingu" className={window.location.pathname === "/" ? "btn btn-light" : "btn"}>
-//         Blog
-//       </a>
-//     ];
-
-//     if (error) {
-//       //do nothing.  this will throw an error when not logged in.  apollo client known issue.
-//       //this if statement will clear it out of the console for now.
-//     };
-
-//     if (!!loggedIn) {
-//       loginMenuItems = [
-//         <Link key="2" className={window.location.pathname === "/" ? "btn btn-light" : "btn"} to="/" onClick={e => this.handleLogout(e)}>
-//           Log Out
-//         </Link>,
-//         <Link key="3" className={window.location.pathname === "/" ? "btn btn-light" : "btn"} to="/" >
-//           <i className="far fa-user fa-2x" />
-//         </Link>
-//       ];
-//     } else {
-//       loginMenuItems = [
-//         <Link key="2" className={window.location.pathname === "/" ? "btn btn-light" : "btn"} to="/signup">
-//           Sign Up
-//         </Link>,
-//         <Link key="3" className={window.location.pathname === "/" ? "btn btn-light" : "btn"} to="/login">
-//           Log In
-//         </Link>
-//       ];
-//     }
-
-//     menu.push(...loginMenuItems);
-
-//     return menu
-//   }
-
-//   render() {
-//     return (
-//       <div className={window.location.pathname === "/" ? "header header-dark" : "header"}>
-//         <div className="header-left">
-//           <div className="nav-logo">
-//             <Link className={window.location.pathname === "/" ? "nav-light" : ""} to="/">CHINGU</Link>
-//           </div>
-//         </div>
-//         <div className="header-right">{this.renderRightNav()}</div>
-//       </div>
-//     );
-//   }
-// }
-
-// export default graphql(currentUserQuery)(Header);
-
-
 import React from "react";
 import { Link } from "react-router-dom";
 import { graphql } from "react-apollo";
 
 import gql from "graphql-tag"; // TODO Remove TEMP
-class Header extends React.Component {
-  state = {
-    auth: !!window.localStorage.getItem("token"), // FIXME
-    // dropDownShowing: false, // TODO remove - dropdown using CSS :hover
-  }
 
-  // componentDidMount() {
-  //   const auth = !!window.localStorage.getItem("token")
+const Header = props => {
+  const token = !!window.localStorage.getItem("token");
 
-  //   if (auth !== this.state.auth) {
-  //     this.setState({ auth })
-  //   }
-  // }
-  
-  handleLogout = e => {
+  const handleLogout = e => {
     e.preventDefault();
     console.log("Logging out");
     window.localStorage.removeItem("token");
@@ -100,15 +16,10 @@ class Header extends React.Component {
     window.location = "/";
   };
 
-  // toggleDropDown = () => this.setState({ dropDownShowing: !this.state.dropDownShowing })
-
-  renderPortalDropDown = () => {
+  const renderPortalDropDown = () => {
     return (
       <div className="header-dropdown">
-        <button 
-          className="header-portal-btn" 
-          // onClick={this.toggleDropDown}
-        >
+        <button className="header-portal-btn" >
           <span>CHOOSE A PORTAL</span>
           <i className="fa fa-chevron-down"/>
         </button>
@@ -126,21 +37,21 @@ class Header extends React.Component {
     )
   }
 
-  renderAuthButtons = () => {
-    const { user } = this.props.data
+  const renderAuthButtons = () => {
+    const { user } = props.data
     let buttons
     
-    if (user) {
+    if (token && user) {
       buttons = <React.Fragment>
         <Link 
           to="/" 
           className="header-btn grey" 
-          onClick={e => this.handleLogout(e)}
+          onClick={e => handleLogout(e)}
         >
           LOG OUT
         </Link>
         {/* <Link className="btn btn-light" to="/"><i className="far fa-user fa-2x" /></Link> */}
-        <img className="avatar" src={user.avatar} alt="user avatar"/>
+        <img className="avatar big" src={user.avatar} alt="user avatar"/>
       </React.Fragment>
     } else {
       buttons = <Link to="/login" className="header-btn green">LOG IN</Link>
@@ -150,10 +61,6 @@ class Header extends React.Component {
   }
   
   
-  render() {
-    // TODO remove
-    this.props.data.user && console.log(`Hello ${this.props.data.user.username}!`)
-
     return (
       <div className="header header-dark">
         <div className="header-left">
@@ -161,12 +68,10 @@ class Header extends React.Component {
             <Link className="nav-light" to="/">CHINGU</Link>
           </div>
         </div>
-        {this.state.auth && this.renderPortalDropDown()}
-        {/* <div className="header-right">{this.renderRightNav()}</div> */}
-        {this.renderAuthButtons()}
+        {props.data.user && renderPortalDropDown()}
+        {renderAuthButtons()}
       </div>
     )
-  }
 }
 
 // TODO remove TEMP
