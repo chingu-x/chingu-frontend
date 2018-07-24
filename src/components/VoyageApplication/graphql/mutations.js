a9ff86fb8ac4bc33ee16ca65f0c5182cd9e2176c
serpient <littlegangwolf@gmail.com>
2018-07-24 13:52:36 -0700

    update voyageApplication mutation key

--- src/components/VoyageApplication/graphql/mutations.js
+++ src/components/VoyageApplication/graphql/mutations.js
@@ -1,7 +1,7 @@
 import gql from "graphql-tag";
 
 export const submitApplication = gql`
-mutation submitVoyageForm($voyage_form: JSON!, $user_form: JSON){
-  submitVoyageForm(input:{voyage_form:$voyage_form, user_form:$user_form})
+mutation submitVoyageForm($voyage_form: JSON!, $new_voyage_user_form: JSON){
+  submitVoyageForm(input:{voyage_form:$voyage_form, new_voyage_user_form:$new_voyage_user_form})
 }
 `
