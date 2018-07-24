a7a72bf6bbca8996cb837a91ff6ad35e5ea5f214
serpient <littlegangwolf@gmail.com>
2018-07-23 17:05:13 -0700

    fixed underline on button

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -8,10 +8,11 @@ const Login = () => (
   <div className="login-container">
     <div className="login-box">
       <div className="login-title">Github Authentication</div>
-      <a
-        href={`https://github.com/login/oauth/authorize?client_id=e015fd9cc874fa5a34bf`}
-      >
-        <button className="github-auth"><img alt="github-icon" className="github-icon" src={'https://i.imgur.com/UBZgVgQ.png'} />Log in with Github</button>
+      <a className="login-link" href={`https://github.com/login/oauth/authorize?client_id=e015fd9cc874fa5a34bf`}>
+        <button className="github-auth">
+          <img alt="github-icon" className="github-icon" src={'https://i.imgur.com/UBZgVgQ.png'} />
+          Log in with Github
+        </button>
       </a>
     </div>
   </div>
