db9fd24f30972fedbd3a9050d87f3f3bc39832e5
serpient <littlegangwolf@gmail.com>
2018-07-25 00:25:34 -0700

    added date formatter

--- src/components/VoyageCard/Info.js
+++ src/components/VoyageCard/Info.js
@@ -1,12 +1,18 @@
 import * as React from 'react';
 
+function dateFormatter (timestamp) {
+  let newDate = new Date(timestamp).toDateString().split(' ');
+  newDate.shift();
+  newDate.pop();
+  return newDate.join(' ');
+}
 const Info = ({startDate, endDate}) => {
   return (
     <div className="card-info">
       <h2>8 Weeks</h2>
       <p>Build to Learn</p>
       <p>
-        {startDate} - {endDate}
+        {dateFormatter(startDate) }  -  { dateFormatter(endDate)}
       </p>
     </div>
   )
