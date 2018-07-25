d8e3e8b613a77905979a6fab5b69b95fc4ad4760
serpient <littlegangwolf@gmail.com>
2018-07-24 20:28:45 -0700

    update weekly check form data

--- src/components/WeeklyCheckin/weeklyCheckin.data.js
+++ src/components/WeeklyCheckin/weeklyCheckin.data.js
@@ -1,9 +1,13 @@
 const weeklyCheckinData = [
     {
         id: 300,
-        type: 'button-health',
+        type: 'button-3-colors',
         question: "How would you rate your team's progress right now?",
-        answers: ['Great', 'Nervous', 'Trouble Ahead!']
+        answers: [
+            {answer: 'Great!', value: 'green', color: '$health-green'}, 
+            {answer: 'Nervous', value: 'yellow', color: '$health-yellow'}, 
+            {answer: 'Trouble Ahead!', value: 'red', color: '$health-red'}, 
+        ]
     },
     {
         id: 301,
