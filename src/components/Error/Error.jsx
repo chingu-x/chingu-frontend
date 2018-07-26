c1a902c768d616a35a17dcb8d1545a2631289acf
serpient <littlegangwolf@gmail.com>
2018-07-25 21:07:39 -0700

    hard coded error message

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -6,7 +6,7 @@ class Error extends React.Component {
       <div className="error-page-container">
         <div className="error-page-modal">
           <img alt="error" className="error-img" src={require('../../assets/error.png')} />
-          <div className="error-message">{this.props.error}</div>
+          <div className="error-message">An Error Has Occurred. Please Try Again</div>
           <hr className="error-hline" />
           <button className="error-go-back-btn">
             <a href={this.props.goBack} >
