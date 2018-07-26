d40ec6c96d079201125134b6a14e1fdbbf248052
serpient <littlegangwolf@gmail.com>
2018-07-26 00:08:53 -0700

    fixed error component goBack btn - take out button, and give A elem the same styling. this is because the hit chance of A wrapped in a button was only over the text itself

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -2,17 +2,16 @@ import * as React from 'react';
 import './Error.css';
 class Error extends React.Component {
   render() {
+    console.log(this.props.goBack);
     return (
       <div className="error-page-container">
         <div className="error-page-modal">
           <img alt="error" className="error-img" src={require('../../assets/error.png')} />
           <div className="error-message">An Error Has Occurred. Please Try Again</div>
           <hr className="error-hline" />
-          <button className="error-go-back-btn">
-            <a href={this.props.goBack} >
+            <a className="error-go-back-btn" href={this.props.goBack} >
               Go back
           </a>
-          </button>
         </div>
       </div>
     )
