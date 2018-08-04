18cefe739c1bcd931d5ec866e3344705a41300e9
serpient <littlegangwolf@gmail.com>
2018-08-03 18:45:48 -0700

    update apollo client URI

--- src/AppGlobalStore.js
+++ src/AppGlobalStore.js
@@ -5,7 +5,7 @@ const STORE_STATE_LOCAL_STORAGE_VERSION = 5;
 // https://d07c9835.ngrok.io/graphql
 // https://api.chingu.io/graphql
 const client = new ApolloClient({
-  uri: 'https://fb.api.chingu.io/graphql',
+  uri: 'https://api.chingu.io/graphql',
   request: operation => operation.setContext({
     headers: {
       authorization: `Bearer ${localStorage.getItem('token')}`,
