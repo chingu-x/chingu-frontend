52c33fccef662087f0b845af656734f35bdf1c5e
The-Vampiire <vampiirecodes@gmail.com>
2018-08-11 16:19:36 -0400

    created GetUser utility component. receive user query and a child component to inject data.

---
+++ src/components/utilities/GetUser.jsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { Query } from "react-apollo";
+
+import Loading from "../Loader/Loader";
+import Error from "../Error/Error";
+
+export default ({ query, component, load }) => { console.log(query, component); return (
+  <Query query={query}>
+  {
+    ({ data, loading, error }) => {
+      if (loading) return load ? <Loading /> : null;
+      if (error) return <Error error={error.message} />;
+      return component(data.user);
+    }
+  }
+  </Query>
+);}
