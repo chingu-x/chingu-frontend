efa5c65c9852c70ede6496a975b3fd01b7fd1b20
The-Vampiire <vampiirecodes@gmail.com>
2018-08-11 16:24:16 -0400

    moved all Landing child components into own dir. Cleaned up to use Component based approach

---
+++ src/components/Landing/components/LandingTop.jsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { Link } from "react-router-dom";
+
+export default ({ user }) =>{ console.log('user: ', user); return (
+  <div className="landing-top" >
+    <div className="tagline-box" >
+      <div className="tagline" >Get out of tutorial purgatory. </div>
+      <div className="tagline--subtext" >Learn how to be a team developer & boost your portfolio.</div>
+      {user
+        ? null
+        : (
+            <Link to="/login" >
+              <button className="big-green-btn">Apply</button>
+            </Link >
+          )
+      }
+    </div>
+    <img className="landing-img" src={require('../../../assets/landingImage.png')} alt="landingImage" />
+  </div>
+);}
