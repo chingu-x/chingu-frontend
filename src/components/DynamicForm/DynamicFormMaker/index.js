d0462da19498524cddf9107758a1234ab9fedad8
The-Vampiire <vampiirecodes@gmail.com>
2018-08-12 04:08:42 -0400

    refactored radio_special_badge to work with dynamic forms

--- src/components/DynamicForm/DynamicFormMaker/index.js
+++ src/components/DynamicForm/DynamicFormMaker/index.js
@@ -32,23 +32,28 @@ export function DynamicFormMaker(questions, onFormChange, form_data) {
       case 'textarea':
         QuestionComponent = questionComponent_textarea;
         break;
+      case 'dropdown_multiple':
       case 'checkbox_2_column':
         QuestionComponent = questionComponent_checkbox_2_column;
         break;
-  // TODO: cases below are not yet supported by back end 8/4
-      case 'dropdown_multiple':
-          QuestionComponent = questionComponent_dropdown_multiple;
-          break;
       case 'radio_special_badge':
         QuestionComponent = questionComponent_radio_special_badge;
-        break;
-      case 'button_3_colors':
-        QuestionComponent = questionComponent_btn_3_options;
         break;
       case 'hidden':
         return null;
       default:
         break;
+
+  // TODO: dropdown_multiple is not functioning
+      // switched to 2 column checkbox for now. easier to work with for the user
+      // case 'dropdown_multiple':
+      //   QuestionComponent = questionComponent_dropdown_multiple;
+      //     break;
+  // TODO: cases below are not yet supported by back end 8/11
+      // case 'button_3_colors':
+      //   QuestionComponent = questionComponent_btn_3_options;
+      //   break;
+  // TODO: unsupported above
     }
     return (
       <div key={'question_' + question.field_name} className="form-QA">
@@ -60,4 +65,5 @@ export function DynamicFormMaker(questions, onFormChange, form_data) {
       </div>
     )
   })
-}
+};
+
