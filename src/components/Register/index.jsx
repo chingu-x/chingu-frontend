cf88100ec704fac40154f13e931d8ab91dede6b4
The-Vampiire <vampiirecodes@gmail.com>
2018-08-12 04:22:06 -0400

    refactored Register component to use new hiddenData DF prop

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -2,12 +2,9 @@ import * as React from "react";
 import DynamicForm from "../DynamicForm";
 import './Register.css';
 // TODO: refactor the styling
-const Register = ({ version, queryString }) => {
-  // pattern for manually adding hidden field data (not found in url)
-  // alternatively could have 'hidden' prop with object of fields
-    // would require parsing QS in the wrapper components (such as Register)
+const Register = ({ version }) => {
+  // get users local timezone
   const timezone = new Date().getTimezoneOffset();
-  const qs = `${queryString}&timezone=${timezone}`;
   return (
     <div className="chingu-application-container">
       <div className="chingu-application-modal">
@@ -15,7 +12,7 @@ const Register = ({ version, queryString }) => {
         <DynamicForm
           purpose="chingu_application"
           version={version}
-          queryString={qs}
+          hiddenData={ { timezone } }
         />
       </div>
     </div>
