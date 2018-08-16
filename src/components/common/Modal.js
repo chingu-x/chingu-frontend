214c69f2ec80ce278dd8d11cc03d2b60f3b3a3b0
thinktwice13 <marioo.krajacic@gmail.com>
2018-08-16 23:45:06 +0200

    Add usage instructions to Modal

--- src/components/common/Modal.js
+++ src/components/common/Modal.js
@@ -1,11 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom";
 
-// export default ({children, onModalClick}) => 
-//   ReactDOM.createPortal(
-//     <div className="modal" onClick={onModalClick}>{children}</div>, 
-//     document.querySelector("#modal-root")
-//   )
+// -- USAGE -- //
+/*
+render <Modal onModalClick={HANDLER} background={BACKGROUND_COLOR} ref={REF_NAME}/>
+Then use this.refs.REF_NAME.open or .close in component methods
+Background color is transparent by default
+ */
 
 export default class extends React.Component {
   constructor(props) {
