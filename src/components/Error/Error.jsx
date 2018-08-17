0f0d0fd0a6a6c1ca14437e4d2c96682ef6861c45
thinktwice13 <marioo.krajacic@gmail.com>
2018-08-17 03:26:06 +0200

    Show errors using Modal component Closes #50

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,22 +1,36 @@
 import * as React from 'react';
 import './Error.css';
-class Error extends React.Component {
-  render() {
-    console.log(this.props.goBack);
-    console.log(this.props.error);
-    return (
-      <div className="error-page-container">
-        <div className="error-page-modal">
-          <img alt="error" className="error-img" src={require('../../assets/error.png')} />
-          <div className="error-message">{this.props.error}</div>
-          <hr className="error-hline" />
-          <a className="error-go-back-btn" href={this.props.goBack || "/profile"} >
-            Go back
-          </a>
-        </div>
-      </div>
-    )
-  }
-}
+import Modal from "../common/Modal"
+// class Error extends React.Component {
+//   render() {
+//     console.log(this.props.goBack);
+//     console.log(this.props.error);
+//     return (
+//       <div className="error-page-container">
+// <div className="error-page-modal">
+//   <img alt="error" className="error-img" src={require('../../assets/error.png')} />
+//   <div className="error-message">{this.props.error}</div>
+//   <hr className="error-hline" />
+//   <a className="error-go-back-btn" href={this.props.goBack || "/profile"} >
+//     Go back
+//   </a>
+// </div>
+//       </div>
+//     )
+//   }
+// }
 
-export default Error;
+// export default Error;
+
+export default ({ goBack, error }) => (
+  <Modal open persist background="white">
+    <div className="error-page-modal">
+      <img alt="error" className="error-img" src={require('../../assets/error.png')} />
+      <div className="error-message">{error}</div>
+      <hr className="error-hline" />
+      <a className="error-go-back-btn" href={goBack || "/profile"} >
+        Go back
+      </a>
+    </div>
+  </Modal>
+)
