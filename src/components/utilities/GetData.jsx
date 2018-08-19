39b58965c9702fea378b8e42af908d2db4bf9581
thinktwice13 <marioo.krajacic@gmail.com>
2018-08-19 20:12:20 +0200

    Add GetData usage example and notes Closes #52

--- src/components/utilities/GetData.jsx
+++ src/components/utilities/GetData.jsx
@@ -3,6 +3,22 @@ import { Query } from "react-apollo"
 import Loader from "../Loader/Loader"
 import Error from "..//Error/Error"
 
+/**
+ * USAGE
+ * export default props => 
+ *  <GetData
+ *  component={COMPONENT}
+ *  query={QUERY}
+ *  variables={VARIABLES}
+ *  { ...props } - props passed to the component that will render
+ *  load - determines if fullscreen loader will be shown while fetching data
+ * />
+ * 
+ * Component receives props passed to it as well as all query results (client, loading, error, data)
+ * 
+ * TODO: Consider controlling auth requirement through requireAuth or similar prop. At the moment handled in Component view. 
+ */
+
 export default ({ component: Component, query, variables, load, ...props }) => (
   <Query query={query} variables={variables}>
     {
