41224aa862e9d9b30d4dffbeef16e4b1080b0ddb
Tony Luo <luo.tony37@gmail.com>
2018-08-20 14:44:21 -0700

    Add project portal banner

--- src/components/Pages/ProjectPortal/index.jsx
+++ src/components/Pages/ProjectPortal/index.jsx
@@ -1,13 +1,28 @@
 import * as React from "react";
 
-function ProjectPortal() {
+class ProjectPortal extends React.Component {
+  render() {
+    return (
+      <div className="project-portal">
+        <Banner />
+        <Toolbar />
+        <Preview />
+        <About />
+        <Team />
+        <ExternalLinks />
+      </div>
+    );
+  }
+}
+
+function Banner() {
   return (
-    <div className="project-portal">
-      <Toolbar />
-      <Preview />
-      <About />
-      <Team />
-      <ExternalLinks />
+    <div className="project-portal__banner">
+      <h1>Chingu</h1>
+      <p>
+        Chingu facilitates global collaboration on projects in a structure
+        focused on improving remote development skills
+      </p>
     </div>
   );
 }
