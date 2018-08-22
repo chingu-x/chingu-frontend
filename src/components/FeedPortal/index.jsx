793bc6345c3b14c9d69e2b89e4022fddeb4fdd1b
thinktwice13 <marioo.krajacic@gmail.com>
2018-08-22 16:47:42 +0200

    Add sidebar items

--- src/components/FeedPortal/index.jsx
+++ src/components/FeedPortal/index.jsx
@@ -1,12 +1,21 @@
-import React from "react"
+import React, { Fragment } from "react"
 import "./FeedPortal.css"
+
+const SidebarBtn = ({ lbl, active }) => (
+  <Fragment>
+    <hr className="hl" />
+    <div
+      className={`sidebar-nav__btn ${active ? "active" : null}`}>
+      {lbl}</div>
+  </Fragment>
+)
 
 class FeedPortal extends React.Component {
   render() {
     return (
       <div className="view-container">
-
         <div className="portal-container">
+
           <aside className="portal-panel__sidebar">
             <div className="sidebar-userinfo__container">
               <img
@@ -17,11 +26,18 @@ class FeedPortal extends React.Component {
                 className="sidebar-userinfo__username">
                 UsernameHere</div>
             </div>
+
+            <SidebarBtn lbl="All News" />
+
+            <hr className="hl" />
+            <labelv className="sidebar-nav__label">Your Teams</labelv>
+            <SidebarBtn lbl="Voyage-4/Bears-Team-11" active />
+            <SidebarBtn lbl="Voyage-2/Turtles-Team-11" />
             <hr className="hl" />
           </aside>
+
           <main className="portal-panel__feed">
             Feed
-            <hr className="hl" />
           </main>
         </div>
       </div>
