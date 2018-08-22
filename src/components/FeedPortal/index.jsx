dade89c239f32429f83abfc94598d762e87c1fe7
thinktwice13 <marioo.krajacic@gmail.com>
2018-08-22 15:28:15 +0200

    Basic newsfeed panels layout

--- src/components/FeedPortal/index.jsx
+++ src/components/FeedPortal/index.jsx
@@ -1,9 +1,16 @@
 import React from "react"
+import "./FeedPortal.css"
 
 class FeedPortal extends React.Component {
   render() {
     return (
-      <div>FeedPortal here</div>
+      <div className="view-container">
+
+        <div className="portal-container">
+          <div className="portal-panel__sidebar">sidebar</div>
+          <div className="portal-panel__feed">feed</div>
+        </div>
+      </div>
     )
   }
 }
