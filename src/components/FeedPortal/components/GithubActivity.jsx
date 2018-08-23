c189073198b9b023a95e4cbef68a51e37097b28d
serpient <littlegangwolf@gmail.com>
2018-08-23 16:16:00 -0700

    starter github activity container

---
+++ src/components/FeedPortal/components/GithubActivity.jsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+
+const GithubActivity = ({ githubActivity }) => {
+    return (
+        <div className="github-activity-container">
+        </div>
+    )
+}
+
+export default GithubActivity;
