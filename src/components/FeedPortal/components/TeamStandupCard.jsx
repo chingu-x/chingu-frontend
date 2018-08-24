b704a76351c4714142c82afb054915cf2f7b8c96
serpient <littlegangwolf@gmail.com>
2018-08-24 14:53:20 -0700

    clean classname ternary and adjust label capitalizing

--- src/components/FeedPortal/components/TeamStandupCard.jsx
+++ src/components/FeedPortal/components/TeamStandupCard.jsx
@@ -19,20 +19,17 @@ const TeamStandupComponent = ({ userStandup }) => {
       break;
   }
   let standupObjects = [
-    { label: 'Health Status:', data: healthStatus },
-    { label: 'Worked on:', data: standup.worked_on },
-    { label: 'Working on next:', data: standup.working_on },
-    { label: 'Blockers:', data: standup.blocked_on }
+    { label: 'health status', data: healthStatus },
+    { label: 'worked on', data: standup.worked_on },
+    { label: 'working on next', data: standup.working_on },
+    { label: 'blockers', data: standup.blocked_on }
   ]
 
   let renderedStandup = standupObjects.map((standup, idx) => {
-    let className = "team-standup-answer"
-    if (standup.label === 'Health Status:') {
-      className = "team-standup-answer team-standup-status--" + userStandup.standup.progress_sentiment;
-    }
+    let className = standup.label === 'health status' ? "team-standup-answer team-standup-status--" + userStandup.standup.progress_sentiment : "team-standup-answer";
     return (
       <div key={"team-standup_" + idx} className="team-standup-data">
-        <div className="team-standup-label">{standup.label}</div>
+        <div className="team-standup-label">{standup.label} :</div>
         <div className={className}>{standup.data}</div>
       </div>
 
