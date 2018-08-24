96f9c64e89dccf78c180b827cfe15038127bc3ac
serpient <littlegangwolf@gmail.com>
2018-08-24 13:07:52 -0700

    update teamStandup card to match passed in object schema

--- src/components/FeedPortal/components/TeamStandupCard.jsx
+++ src/components/FeedPortal/components/TeamStandupCard.jsx
@@ -2,8 +2,9 @@ import * as React from 'react';
 import './TeamStandupCard.css';
 
 const TeamStandupComponent = ({ userStandup }) => {
+  let standup = userStandup.standup;
   let healthStatus;
-  switch (userStandup.progress_sentiment) {
+  switch (standup.progress_sentiment) {
     case 'red':
       healthStatus = 'Trouble Ahead!';
       break;
@@ -19,20 +20,20 @@ const TeamStandupComponent = ({ userStandup }) => {
   }
   let standupObjects = [
     { label: 'Health Status:', data: healthStatus },
-    { label: 'Worked on:', data: userStandup.worked_on },
-    { label: 'Working on next:', data: userStandup.working_on },
-    { label: 'Blockers:', data: userStandup.blocked_on }
+    { label: 'Worked on:', data: standup.worked_on },
+    { label: 'Working on next:', data: standup.working_on },
+    { label: 'Blockers:', data: standup.blocked_on }
   ]
 
-  let standup = standupObjects.map((standup, idx) => {
+  let renderedStandup = standupObjects.map((standup, idx) => {
     let className = "team-standup-answer"
     if (standup.label === 'Health Status:') {
-      className = "team-standup-answer team-standup-status--" + userStandup.progress_sentiment;
+      className = "team-standup-answer team-standup-status--" + userStandup.standup.progress_sentiment;
     }
     return (
-      <div className="team-standup-data">
-        <div key={idx} className="team-standup-label">{standup.label}</div>
-        <div key={idx} className={className}>{standup.data}</div>
+      <div key={"team-standup_" + idx} className="team-standup-data">
+        <div className="team-standup-label">{standup.label}</div>
+        <div className={className}>{standup.data}</div>
       </div>
 
     )
@@ -40,7 +41,7 @@ const TeamStandupComponent = ({ userStandup }) => {
 
   return (
     <div className="team-standup-card-container">
-      {standup}
+      {renderedStandup}
     </div>
   )
 }
