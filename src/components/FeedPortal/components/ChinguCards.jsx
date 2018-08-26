01060cb9b20a29fa041ed828aafedaf45cc2b05d
The-Vampiire <vampiirecodes@gmail.com>
2018-08-26 00:04:58 -0400

    routed NewsfeedVoyage to /voyage/application/ID

--- src/components/FeedPortal/components/ChinguCards.jsx
+++ src/components/FeedPortal/components/ChinguCards.jsx
@@ -2,11 +2,20 @@ import * as React from "react";
 import './ChinguCards.css';
 import { Link } from "react-router-dom";
 
-export const NewsfeedVoyage = ({ voyage }) => {
+export const NewsfeedVoyage = ({ voyage: { id, title } }) => {
   return (
-    <Link to="/voyage" className="newsfeed-chingu-card--container chingu-card--green">
-      <img className="newsfeed-chingu-card--icon" src={require('../../../assets/green alert.png')} alt="green" />
-      <div className="newsfeed-voyage-text">{voyage.title} {voyage.id} Applications are open! Click here to apply.</div>
+    <Link
+      to={`/voyage/application/${id}`}
+      className="newsfeed-chingu-card--container chingu-card--green"
+    >
+      <img
+        className="newsfeed-chingu-card--icon"
+        src={require('../../../assets/green alert.png')}
+        alt="green"
+      />
+      <div className="newsfeed-voyage-text">
+        {title} {id} Applications are open! Click here to apply.
+      </div>
     </Link>
   )
 }
