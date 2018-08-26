d20084f2e489c0e7443d04ffea0b49ddc080fcde
thinktwice13 <marioo.krajacic@gmail.com>
2018-08-26 21:21:11 +0200

    Update Landing page - hide Apply button for authed users

--- src/components/Landing/components/LandingTop.jsx
+++ src/components/Landing/components/LandingTop.jsx
@@ -5,16 +5,22 @@ import voyagesQuery from "../../VoyagePortal/graphql/voyagesQuery"
 export default props => (
   <div className="landing-top" >
     <div className="tagline-box" >
-      <div className="tagline" >Get out of tutorial purgatory. </div>
-      <div className="tagline--subtext" >Learn how to be a team developer & boost your portfolio.</div>
-      <button
-        className="big-green-btn"
-        onClick={props.onApplyClick}
-        // Prefetch /voyage route only on auth
-        onMouseOver={() => localStorage.token && client.query({ query: voyagesQuery })}
-      >
-        Apply
+      {
+        !props.authed &&
+        <React.Fragment>
+
+          <div className="tagline" >Get out of tutorial purgatory. </div>
+          <div className="tagline--subtext" >Learn how to be a team developer & boost your portfolio.</div>
+          <button
+            className="big-green-btn"
+            onClick={props.onApplyClick}
+            // Prefetch /voyage route only on auth
+            onMouseOver={() => localStorage.token && client.query({ query: voyagesQuery })}
+          >
+            Apply
       </button>
+        </React.Fragment>
+      }
     </div>
     <img className="landing-img" src={require('../../../assets/landingImage.png')} alt="landingImage" />
   </div >
