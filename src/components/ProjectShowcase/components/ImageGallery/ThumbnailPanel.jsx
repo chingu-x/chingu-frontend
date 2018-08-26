4581911195445ad321f85ff822d0ff9e08c2bb45
Tony Luo <luo.tony37@gmail.com>
2018-08-26 15:39:24 -0700

    Add propTypes and defaultProps to ThumbnailPanel

--- src/components/ProjectShowcase/components/ImageGallery/ThumbnailPanel.jsx
+++ src/components/ProjectShowcase/components/ImageGallery/ThumbnailPanel.jsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
+import propTypes from 'prop-types';
 import Thumbnails from './Thumbnails';
 
-class ThumbnailPanel extends React.Component {
-  render() {
+function ThumbnailPanel(props) {
     return (
       <div className="project-portal__preview-color">
         <div className="project-portal__preview">
@@ -10,13 +10,22 @@ class ThumbnailPanel extends React.Component {
             type="button"
             value="Update Images"
             className="project-btn"
-            onClick={(e) => this.props.toggleImgModal(e)}
+            onClick={(e) => props.toggleImgModal(e)}
           />
-          <Thumbnails files={this.props.files} />
+          <Thumbnails files={props.files} />
         </div>
       </div>
     );
-  }
 }
+
+ThumbnailPanel.propTypes = {
+  toggleImgModal: propTypes.func,
+  files: propTypes.array,
+};
+
+ThumbnailPanel.defaultProps = {
+  toggleImgModal: console.log,
+  files: []
+};
 
 export default ThumbnailPanel;
