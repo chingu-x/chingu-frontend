74814ee802a5e5f383529f8625f15bb1d12a0f78
Tony Luo <luo.tony37@gmail.com>
2018-08-26 13:27:57 -0700

    Add responsibility comment to main page

--- src/components/ProjectShowcase/index.jsx
+++ src/components/ProjectShowcase/index.jsx
@@ -6,6 +6,12 @@ import Toolbar from './components/Toolbar';
 import EditProjectForm from './components/EditProjectForm';
 import './ProjectShowcase.css';
 
+/*
+
+This component should only be concerned with the overall layout of the page and whether it is being edited or not. It should also control the permissions for edits based on if user is logged in and if they are a part of this project.
+
+*/
+
 class ProjectShowcase extends React.Component {
   state = {
     isEditing: false
