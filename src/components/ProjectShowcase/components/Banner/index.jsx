8d2143c8509c358e0acf05d64f2d7a0489ba58c0
Tony Luo <luo.tony37@gmail.com>
2018-08-27 00:08:16 -0700

    Add functionality to edit Banner text

--- src/components/ProjectShowcase/components/Banner/index.jsx
+++ src/components/ProjectShowcase/components/Banner/index.jsx
@@ -1,27 +1,51 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 
-function Banner({editable, title, elevatorPitch}) {
-  return (
-    <div className="project-portal__banner">
-      <h1>{title}</h1>
-      <p>
-        {elevatorPitch}
-      </p>
-    </div>
-  );
-}
 
-Banner.propTypes = {
-  editable: PropTypes.bool,
-  title: PropTypes.string,
-  elevatorPitch: PropTypes.string
-}
+class Banner extends React.Component {
+  static propTypes = {
+    editable: PropTypes.bool,
+    title: PropTypes.string,
+    elevatorPitch: PropTypes.string
+  }
 
-Banner.defaultProps = {
-  editable: false,
-  title: '',
-  elevatorPitch: ''
-};
+  static defaultProps = {
+    editable: false,
+    title: '',
+    elevatorPitch: ''
+  }
+
+  state = {
+    isEditing: false,
+    title: this.props.title,
+    elevatorPitch: this.props.elevatorPitch
+  };
+
+  toggleEdit = () => {
+    this.setState({ isEditing: !this.state.isEditing});
+  }
+
+  handleChange = (e) => {
+    const { value, name } = e.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  render() {
+    const { isEditing, title, elevatorPitch} = this.state;
+    const { editable } = this.props;
+
+    return (
+      <div className="project-portal__banner">
+        { editable && <button style={{ margin: '10px 20px'}} onClick={this.toggleEdit}>{isEditing ? 'Done' : 'Edit'}</button> }
+        <h1>{isEditing ? <input name="title" value={title} onChange={this.handleChange} /> : title}</h1>
+        <p>
+          {isEditing ? <input name="elevatorPitch" value={elevatorPitch} onChange={this.handleChange} /> : elevatorPitch}
+        </p>
+      </div>
+    );
+  }
+}
 
 export default Banner;
