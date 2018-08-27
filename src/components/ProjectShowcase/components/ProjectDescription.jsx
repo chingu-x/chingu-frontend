91c0e79e193d1e36499c529c694fd8b246147027
Tony Luo <luo.tony37@gmail.com>
2018-08-26 21:19:36 -0700

    Update ProjectDescription to be an Editable

--- src/components/ProjectShowcase/components/ProjectDescription.jsx
+++ src/components/ProjectShowcase/components/ProjectDescription.jsx
@@ -1,29 +1,49 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import ReactMarkdown from 'react-markdown';
 
 let md = `
-## Chingu
-August 16, 2018
+**Tell us about your project**
 
-Chingu is building a global collaboration platform and coding-cohort generator. A Chingu-cohort is a build-to-learn community, where motivated developers from around the world, are organized into teams to build a project in 8 weeks. We call these cohorts, Voyages. Chingu so far has been a collaboration platform without an actual “platform”.
+What inspired you? What was the problem that you were trying to solve? What did you learn by completing this project?
 
-For each of the 50+ cohorts we’ve ran - which includes people from 135 different countries - we’ve managed to stay afloat using several unconnected tools such as forms, spreadsheets, slack, github repos, and trellos. With this many people, things tend to get a little messy and chaotic -- for all of us. Our project for this competition aims to fix this and to allow our thriving underground community to emerge.
-
-### Inspiration
-Chingu is building a global collaboration platform and coding-cohort generator. A Chingu-cohort is a build-to-learn community, where motivated developers from around the world, are organized into teams to build a project in 8 weeks. We call these cohorts, Voyages. Chingu so far has been a collaboration platform without an actual “platform”.
-
-For each of the 50+ cohorts we’ve ran - which includes people from 135 different countries - we’ve managed to stay afloat using several unconnected tools such as forms, spreadsheets, slack, github repos, and trellos. With this many people, things tend to get a little messy and chaotic -- for all of us. Our project for this competition aims to fix this and to allow our thriving underground community to emerge.
-
-### What we learned
-That people are supercharged when optimally placed in an engaged community with shared goals. And that this is especially true for learners.
+(Feel free to use Markdown)
 `
 
-function ProjectDescription(props) {
-  return (
-    <div className="project-portal__about">
-      <ReactMarkdown source={md} />
-    </div>
-  );
+class ProjectDescription extends React.Component {
+  static propTypes = {
+    text: PropTypes.string,
+    editable: PropTypes.bool
+  }
+
+  static defaultProps = {
+    text: md,
+    editable: false
+  }
+
+  state = {
+    isEditing: false
+  };
+
+  toggleEdit = () => {
+    this.setState({ isEditing: !this.state.isEditing});
+  }
+
+  render() {
+    const { isEditing } = this.state;
+    const { text } = this.props;
+
+    return (
+      <div className="project-portal__about">
+        <button style={{ margin: '10px 20px'}}onClick={this.toggleEdit}>{isEditing ? 'Done' : 'Edit'}</button>
+        { 
+          isEditing
+          ? <textarea style={{width: '100%', minHeight: '500px'}}>{text}</textarea>
+          : <ReactMarkdown source={text} />
+        }
+      </div>
+    );
+  }
 }
 
 export default ProjectDescription;
