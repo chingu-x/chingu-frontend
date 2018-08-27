a522bb28abb8a4cdb5e778cbd11c88c25d7c04e4
Tony Luo <luo.tony37@gmail.com>
2018-08-27 12:02:18 -0700

    Update ProjectDescription to work with Mutation

--- src/components/ProjectShowcase/components/ProjectDescription.jsx
+++ src/components/ProjectShowcase/components/ProjectDescription.jsx
@@ -1,15 +1,16 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import ReactMarkdown from 'react-markdown';
+import * as React from "react";
+import PropTypes from "prop-types";
+import { gql } from "apollo-boost";
+import { Mutation } from "react-apollo";
+import ReactMarkdown from "react-markdown";
 
-
-/*
-
-TODO:
-- Make mutations to update Project.description
-- Styling
-
-*/
+const updateProject = gql`
+  mutation updateProject($text: String) {
+    updateProject(text: $text) @client {
+      text
+    }
+  }
+`;
 
 const md = `
 **Tell us about your project**
@@ -17,50 +18,81 @@ const md = `
 What inspired you? What was the problem that you were trying to solve? What did you learn by completing this project?
 
 (Feel free to use Markdown)
-`
+`;
 
 class ProjectDescription extends React.Component {
   static propTypes = {
     text: PropTypes.string,
     editable: PropTypes.bool
-  }
+  };
 
   static defaultProps = {
-    text: '',
+    text: "",
     editable: false
-  }
+  };
 
   state = {
     isEditing: false,
-    text: this.props.text || md,
+    text: this.props.text || md
   };
 
-  toggleEdit = () => {
-    this.setState({ isEditing: !this.state.isEditing});
-  }
+  toggleEditWithSave = mutation => {
+    let { isEditing } = this.state;
 
-  handleChange = (e) => {
+    this.setState({ isEditing: !isEditing });
+
+    if (isEditing) {
+      this.handleMutation(mutation);
+    }
+  };
+
+  handleChange = e => {
     const { value, name } = e.target;
 
     this.setState({
       [name]: value
     });
-  }
+  };
+
+  handleMutation = mutation => {
+    const { text } = this.state;
+    mutation({ variables: { text } });
+  };
 
   render() {
     let { isEditing, text } = this.state;
     let { editable } = this.props;
 
+    // FIXME: Error handling
+    // FIXME: Save data to cache
     return (
-      <div className="project-portal__about">
-        { editable && <button style={{ margin: '10px 20px'}}onClick={this.toggleEdit}>{isEditing ? 'Done' : 'Edit'}</button> }
-        
-        { 
-          isEditing
-          ? <textarea name="text" value={this.state.text} style={{width: '100%', minHeight: '500px'}} onChange={this.handleChange} />
-          : <ReactMarkdown source={this.state.text} />
-        }
-      </div>
+      <Mutation mutation={updateProject}>
+        {(updateProject, { data }) => {
+          return (
+            <div className="project-portal__about">
+              {editable && (
+                <button
+                  style={{ margin: "10px 20px" }}
+                  onClick={() => this.toggleEditWithSave(updateProject)}
+                >
+                  {isEditing ? "Done" : "Edit"}
+                </button>
+              )}
+
+              {isEditing ? (
+                <textarea
+                  name="text"
+                  value={this.state.text}
+                  style={{ width: "100%", minHeight: "500px" }}
+                  onChange={this.handleChange}
+                />
+              ) : (
+                <ReactMarkdown source={this.state.text} />
+              )}
+            </div>
+          );
+        }}
+      </Mutation>
     );
   }
 }
