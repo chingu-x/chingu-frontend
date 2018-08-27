4cfed82054d3df567bd4bdd9ddfa3f3c07bb8699
serpient <littlegangwolf@gmail.com>
2018-08-27 14:58:22 -0700

    hook project data to sidebar components

--- src/components/ProjectShowcase/components/ProjectSideBar.jsx
+++ src/components/ProjectShowcase/components/ProjectSideBar.jsx
@@ -1,80 +1,82 @@
 import * as React from "react";
 
-function ProjectSideBar() {
+const ProjectSideBar = ({ project }) => {
+  console.log(project)
   return (
     <div className="project-side-panel">
-      <ExternalLinks />
-      <hr className="project-side-panel--hline" />
-      <TechStack />
-      <hr className="project-side-panel--hline" />
-      <Tags />
-      <hr className="project-side-panel--hline" />
-      <Team />
+      <ExternalLinks data={project} />
+      {project.skills && project.skills.length > 0 && <TechStack teckstack={project.skills} />}
+      {project.tags && project.tags.length > 0 && <Tags tags={project.tags} />}
+      {project.users && <Team users={project.users} />}
     </div>
-
   )
 }
 
-function ExternalLinks() {
+const ExternalLinks = ({ data: { github_url, project_url } }) => {
+  return (
+    <React.Fragment>
+      <div className="project-subcategory">
+        <h1 className="project-subcategory-title">Links</h1>
+        <div className="project-buttons-container">
+          <a className="project-buttons" target="_blank" href={github_url}>GitHub Repo</a>
+          <a className="project-buttons" target="_blank" href={project_url}>Live Preview</a>
+        </div>
+      </div>
+      <hr className="project-side-panel--hline" />
+    </React.Fragment>
+  );
+}
+
+const TechStack = ({ skills }) => {
+  return (
+    <React.Fragment>
+      <div className="project-subcategory">
+        <h1 className="project-subcategory-title">skills</h1>
+        <div className="project-tags-container">
+          {skills.map((skill, idx) => {
+            return <li key={idx} className="project-tags">{skill}</li>
+          })}
+        </div>
+      </div>
+      <hr className="project-side-panel--hline" />
+    </React.Fragment>
+  )
+}
+
+const Tags = ({ tags }) => {
+  return (
+    <React.Fragment>
+      <div className="project-subcategory">
+        <h1 className="project-subcategory-title">tags</h1>
+        <div className="project-tags-container">
+          {tags.map((tag, idx) => {
+            return <li key={idx} className="project-tags">{tag}</li>
+          })}
+        </div>
+      </div>
+      <hr className="project-side-panel--hline" />
+    </React.Fragment>
+  )
+}
+
+const Team = ({ users }) => {
   return (
     <div className="project-subcategory">
-      <h1 className="project-subcategory-title">Links</h1>
-      <div className="project-buttons-container">
-        <a className="project-buttons" href="#">GitHub Repo</a>
-        <a className="project-buttons" href="#">Live Preview</a>
+      <h1 className="project-subcategory-title">Team</h1>
+      <div className="project-team-container">
+        {users.map((user, idx) => {
+          return <Profile user={user} key={idx} />
+        })}
       </div>
     </div>
   );
 }
-  
-function TechStack() {
-  return (
-    <div className="project-subcategory">
-      <h1 className="project-subcategory-title">skills</h1>
-      <div className="project-tags-container">
-        <li className="project-tags">React</li>
-        <li className="project-tags">React</li>
-        <li className="project-tags">React</li>
-        <li className="project-tags">React</li>
-        <li className="project-tags">React</li>
-        <li className="project-tags">React</li>
-      </div>
-    </div>
-  )
-}
 
-function Tags() {
-  return (
-    <div className="project-subcategory">
-      <h1 className="project-subcategory-title">tags</h1>
-      <div className="project-tags-container">
-        <li className="project-tags">Developer Tools</li>
-      </div>
-    </div>
-  )
-}
-  
-function Team() {
-  return (
-    <div className="project-portal__team">
-      <h3>Team</h3>
-      <ul>
-        <li>
-          <Profile />
-        </li>
-        <li>
-          <Profile />
-        </li>
-      </ul>
-    </div>
-  );
-}
-  
-function Profile() {
+const Profile = ({ user: { username, avatar } }) => {
   return (
     <div className="project-portal__profile">
-      <img src="https://via.placeholder.com/50x50" alt="" />
-      <p>Cool Cat</p>
+      <img className="project-portal__user-avatar" src={avatar} alt="team-pic" />
+      <p className="project-portal__user-username">{username}</p>
     </div>
   );
 }
