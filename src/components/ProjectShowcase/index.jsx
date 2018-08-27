ef072c6ec1395d40bf79b3b5a5b5c61bf7cbb1a3
Tony Luo <luo.tony37@gmail.com>
2018-08-26 22:58:49 -0700

    Add query for user and project
    Need to update query when we are able to query for a single project

--- src/components/ProjectShowcase/index.jsx
+++ src/components/ProjectShowcase/index.jsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import Banner from './components/Banner';
 import ProjectSideBar from './components/ProjectSideBar';
 import ProjectDescription from './components/ProjectDescription';
+import { gql } from 'apollo-boost';
 import './ProjectShowcase.css';
 
 /*
@@ -9,6 +10,30 @@ import './ProjectShowcase.css';
 This component should only be concerned with the overall layout of the page and whether it is editable.
 
 */
+const getProjectAndUser = gql`
+  query getProjectAndUser($title: String) {
+    user {
+      id
+    }
+    projects(title: $title) { # FIXME[1]: Update query for retrieving a single Project based on route params
+      id
+      title
+      description
+      project_url
+      github_url
+      users {
+        id
+        username
+        avatar
+      }
+      skills{
+        id
+        name
+      }
+    }
+  }
+`
+
 class ProjectShowcase extends React.Component {
   state = {
     editable: false
@@ -23,6 +48,12 @@ class ProjectShowcase extends React.Component {
             <ProjectSideBar />
         </div>
       </div>
+                  <ProjectSideBar />
+              </div>
+            </div>
+          );
+        }}
+      </Query>
     );
   }
 }
