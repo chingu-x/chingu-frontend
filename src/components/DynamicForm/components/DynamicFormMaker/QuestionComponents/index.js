81343b29a14258f7ecbdc663b297e39d5bd13fd4
The-Vampiire <vampiirecodes@gmail.com>
2018-08-28 14:36:19 -0400

    added custom components (voyage tier select, team progress sentiment)

--- src/components/DynamicForm/components/DynamicFormMaker/QuestionComponents/index.js
+++ src/components/DynamicForm/components/DynamicFormMaker/QuestionComponents/index.js
@@ -5,6 +5,9 @@ import radio from "./radio";
 import text from "./text";
 import textarea from "./textarea";
 
+import team_progress_sentiment_buttons from "./team_progress_sentiment_buttons.jsx";
+import voyage_application_tier_select from "./voyage_application_tier_select";
+
 export default {
   checkbox,
   checkbox_2_column,
@@ -12,4 +15,7 @@ export default {
   radio,
   text,
   textarea,
+// -- CUSTOM -- //
+  voyage_application_tier_select,
+  team_progress_sentiment_buttons,
 };
