8267374852f91cf30019c76d62f3fd4aa47889d7
serpient <littlegangwolf@gmail.com>
2018-08-28 15:34:52 -0700

    basic setup for editable text fields

--- src/components/UserProfile/UserSideBar.jsx
+++ src/components/UserProfile/UserSideBar.jsx
@@ -1,11 +1,24 @@
 import * as React from "react";
 import './UserSideBar.css'
+import EditableTextField from '../utilities/EditableTextField';
+import { gql } from "apollo-boost"
 /**
  * TODO:
  * 1. remove hardcoded data
  * 2. factor out html components
  * 3. convert class to function
  **/
+
+const userUpdate = gql`
+mutation userUpdate($background: String, $interests: String, $coding_history: String) {
+  updateProject(background: $title, interests: $elevator_pitch, coding_history: $coding_history) {
+    id
+    background
+    interests
+    coding_history
+  }
+}
+`;
 
 const USER_INFO_DOM_ELEMENTS = [
   {
@@ -15,7 +28,7 @@ const USER_INFO_DOM_ELEMENTS = [
   },
   {
     divClassName: 'user-coding-history',
-    schemaKey: 'codingHistory',
+    schemaKey: 'coding_history',
     desc: 'Coding History',
   },
   {
@@ -25,38 +38,39 @@ const USER_INFO_DOM_ELEMENTS = [
   },
 ];
 
-const UserSideBar = ({ user }) => {
-  let userInfoDOM = USER_INFO_DOM_ELEMENTS.map(elem => {
-    if (user[elem.schemaKey] && user[elem.schemaKey].length > 0) {
-      return (
-        <div className={elem.divClassName}>
-          <h1 className="user-sidebar-subcategory">{elem.desc}</h1>
-          <p>{user[elem.schemaKey]}</p>
-        </div>
-      )
-    }
-    else {
-      return null;
-    }
-  });
-
-  let skillDOM = null;
-  if (user.skills && user.skills.length > 0) {
-    skillDOM = (
-      <div className="user-skills">
-        <h1 className="user-sidebar-subcategory">skills</h1>
-        <ul>
-          {user.skills.map(elem => (<li>{elem}</li>))}
-        </ul>
-      </div>
-    )
+class UserSideBar extends React.Component {
+  state = {
+    isEditing: false,
+    user: {}
+  }
+  componentDidMount() {
+    this.setState({ user: this.props.user });
   }
 
-  // once links are integrated again, render this
-  // check for fb/linkedin/gitub
-  let linkDOM = null;
-  if (user) {
-    linkDOM = (
+  renderUserInfo = () => {
+    let user = this.state.user;
+    return USER_INFO_DOM_ELEMENTS.map(elem => {
+      (user[elem.schemaKey] && user[elem.schemaKey].length > 0) &&
+        <EditableTextField
+          large
+          mutation={userUpdate}
+          mutationName="userUpdate"
+          fieldName={elem.schemaKey}
+          fieldData={user[elem.schemaKey]}
+          hasPermission={this.state.isEditing}
+          component={(
+            <div className={elem.divClassName}>
+              <h1 className="user-sidebar-subcategory">{elem.desc}</h1>
+              <p>{user[elem.schemaKey]}</p>
+            </div>
+          )}
+        />
+    });
+  }
+
+  renderLinks = () => {
+    let user = this.state.user;
+    return (
       <div className="user-links">
         <h1 className="user-sidebar-subcategory">links</h1>
         <ul>
@@ -70,36 +84,50 @@ const UserSideBar = ({ user }) => {
     )
   }
 
-  return (
-    <div className="user-profile-container-personal">
-      <header className="user">
-        <div className="photobox">
-          <img
-            className="user-photo"
-            src={user ? user.avatar : require('../../assets/blank image.png')}
-            alt="userprofile"
-          />
-          <p>{user.username}</p>
-          <p>Based in {user.country}</p>
-        </div>
-        <ul className="positions">
-          {/* <li className="position">
+  render() {
+    let { user } = this.state.user;
+    return (
+      <div className="user-profile-container-personal">
+        <header className="user">
+          <div className="photobox">
+            <img
+              className="user-photo"
+              src={user ? user.avatar : require('../../assets/blank image.png')}
+              alt="userprofile"
+            />
+            <p>{user.username}</p>
+            <p>Based in {user.country}</p>
+          </div>
+          <ul className="positions">
+            <li className="position">
               <span>
                 <i className="fas fa-check" />
-              </span>Designer
-            </li> */}
-          <li className="position">
-            <span>
-              <i className="fas fa-check" />
-            </span>Programmer
-            </li>
-        </ul>
-      </header>
-      {skillDOM}
-      {userInfoDOM}
-      {linkDOM}
-    </div>
-  );
+              </span>Programmer
+              </li>
+          </ul>
+        </header>
+        {this.renderUserInfo()}
+        {this.renderLinks()}
+      </div>
+    );
+  }
+
 }
 
 export default UserSideBar;
+
+
+  // let skillDOM = null;
+  // if (user.skills && user.skills.length > 0) {
+  //   skillDOM = (
+  //     <div className="user-skills">
+  //       <h1 className="user-sidebar-subcategory">skills</h1>
+  //       <ul>
+  //         {user.skills.map(elem => (<li>{elem}</li>))}
+  //       </ul>
+  //     </div>
+  //   )
+  // }
+
+  // once links are integrated again, render this
+  // check for fb/linkedin/gitub
