4002f90a3cff94cb07803e17404c3123aa786f23
The-Vampiire <vampiirecodes@gmail.com>
2018-08-28 02:41:38 -0400

    fixed dynamic form query import in VoyageCards/Action

--- src/components/VoyageCard/Action.js
+++ src/components/VoyageCard/Action.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { Link } from "react-router-dom"
 import { client } from "../../index"
-import dynamicFormQuery from "../DynamicForm/graphql/dynamicFormQuery"
+import dynamicFormQuery from "../DynamicForm/dynamicFormQuery"
 import voyagesQuery from "../VoyagePortal/graphql/voyagesQuery"
 
 const voyageActionSwitch = (userStatus, routeParam) => {
