23e6622bcc27219bb68de1efad81520f0ea56e80
The-Vampiire <vampiirecodes@gmail.com>
2018-08-28 20:36:14 -0400

    validation for radio and dropdown

--- src/components/DynamicForm/components/DynamicFormMaker/QuestionComponents/dropdown.jsx
+++ src/components/DynamicForm/components/DynamicFormMaker/QuestionComponents/dropdown.jsx
@@ -4,27 +4,30 @@ export default (
   { field_name, options },
   onFormChange,
   form_data,
-) => (
-  <select
-    className="form-dropdown"
-    value={form_data[field_name]}
-    name={field_name}
-    type="dropdown"
-    onChange={e => onFormChange(e)}
-    multiple={false}
-  >
-    {
-      options.map(
-        (answer, index) => (
-          <option
-            className="form-answer"
-            value={answer}
-            key={'dropdown_' + field_name + '_' + index}
-          >
-            {answer}
-          </option>
-        ),
-      )
-    }
-  </select>
-);
+) => {
+  const value = form_data[field_name];
+  return (
+    <select
+      className="form-dropdown"
+      value={value}
+      name={field_name}
+      type="dropdown"
+      onChange={onFormChange}
+      multiple={false}
+    >
+      {
+        options.map(
+          (answer, index) => (
+            <option
+              className="form-answer"
+              value={answer}
+              key={'dropdown_' + field_name + '_' + index}
+            >
+              {answer}
+            </option>
+          ),
+        )
+      }
+    </select>
+  );
+}
