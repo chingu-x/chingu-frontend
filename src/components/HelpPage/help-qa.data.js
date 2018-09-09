da232890447c742a74d2c443af2872f869c98973
serpient <littlegangwolf@gmail.com>
2018-09-08 19:30:47 -0700

    setup help QA

---
+++ src/components/HelpPage/help-qa.data.js
@@ -0,0 +1,107 @@
+export const HelpQA = [
+  {
+    category: 'Chingu Code of Conduct',
+    qa_set: [
+      {
+        question: 'What is Chingu?',
+        answer: `Chingu is building a global collaboration capital for 21st century tech-learners** 
+                to gain experience, meet friends and generate impact through building projects. 
+                Chingu’s mission is to empower the planet’s tech-learners with 21st century skills 
+                by facilitating diverse collaboration experiences and build-to-learn projects.
+                **Developers, designers, data scientists, and digital marketers can all find a home here. 
+                `
+      },
+      {
+        question: 'What is a Voyage?',
+        answer: `A Voyage is an ambitious community with shared goals and an opportunity to gain experience 
+                  learning and building with a team. Those that are accepted are organized into remote teams, 
+                  given prompts and various tools to aid them accelerate their learning. 
+                `
+      },
+      {
+        question: 'How long is the Voyage?',
+        answer: `A Voyage lasts 8 weeks. `
+      },
+      {
+        question: 'Where is Chingu located? ',
+        answer: `Chingu and all the Voyages we run are 100% remote.`
+      },
+      {
+        question: 'How many people are in a typical Voyage?',
+        answer: `A Voyage has between 200-300 people, with an average of 75 different countries represented. `
+      },
+      {
+        question: 'What are my Voyage options?',
+        answer: `See the current Voyage list for dates.`
+      },
+      {
+        question: 'Is this program only for Full-time members? ',
+        answer: `No, Voyages are open to both Full-time and Part-time learners. `
+      },
+      {
+        question: 'What are the technical qualifications?',
+        answer: `You need to have built something. Your very first project should not be a team project. 
+                If you don’t have a finished project, go build and then apply. `
+      },
+      {
+        question: 'What are the non-technical qualifications?',
+        answer: `Soft skills are the greatest skill we can learn, and are paramount to any team’s success. 
+                We don’t expect you to be a master, but we do expect you to have read the prep material on soft skills. `
+      },
+      {
+        question: 'How much does it cost?',
+        answer: `Voyages are free. We are developing another product that will have a price, but that is still in development. `
+      },
+      {
+        question: 'Who is the typical Chingu Voyage applicant?',
+        answer: `Open to collaboration, accountable, and hungry to level-up their skills. ${<br />} As far as skill and experience 
+                level, if 0 is just started to learn today and 100 is got a developer job, Chingu members come from the 
+                15 - 150 range. New learners who have built a project or two → experienced professional developers 
+                learning new skills (new frameworks, leadership, mentorship). 
+                `
+      }
+    ]
+  },
+  {
+    category: 'Curriculum',
+    qa_set: [
+      {
+        question: 'What skills will I learn at Chingu?',
+        answer: `In a typical Voyage, you can expect to have the opportunity to earn various Hard and Soft skills. 
+                ${<br />}
+                ${<br />}
+                ${<bold>Hard skills: </bold>}
+                  Git/github
+                  Accelerated 
+                  ${<br />}
+                  ${<br />}
+                  ${<bold>Soft skills: </bold>}
+                  Leadership
+                  Time management 
+                  Collaboration
+                  Confidence 
+                  Empathy
+                  Conflict resolution 
+                  Adaptability
+                  Creativity  
+                `
+      },
+      {
+        question: 'Why can we use any tech-stack?',
+        answer: `Yes, Chingu is tech-stack agnostic and the team decides together what tech-stack to use.
+                ${<br />}
+                 A big portion of our team-formation algorithm is connecting people who have similar experience 
+                 levels and tech-stack preferences.
+                 ${<br />}
+                Most people who join Chingu thus far are into Web-Development, and the most common stack is MERN 
+                (MongoDB, Express, React, Node) for the more experienced and HTML/CSS/Basic Javascript for the newer learners. 
+                `
+      },
+      {
+        question: `What's a typical day like in the Voyage?`,
+        answer: `This really depends on the team and particular Voyage, but building something or learning what 
+                is needed to build something likely on an average day. All with the support of a community doing the same thing. `
+      },
+    ]
+  }
+]
