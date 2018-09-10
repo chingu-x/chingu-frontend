41d600b57ccc031bba0b632ce7ec998b269dcc00
serpient <littlegangwolf@gmail.com>
2018-09-10 01:46:41 -0700

    update Error component to only render sub-message

--- src/components/Ticketbox/components/Error.jsx
+++ src/components/Ticketbox/components/Error.jsx
@@ -1,19 +1,14 @@
 import * as React from "react";
 import BackBtn from './BackBtn';
 
-const Error = ({ category, imgSrc, switchRenderedType }) => (
-    <div className="bug-suggestion-box">
-        <div className={`box-color color--${category}`}>
-            <img className="box-icon" alt="icon" src={imgSrc} />
-        </div>
-        <div className="ticketbox-success">
-            Well, this is embarassing.
+const Error = ({ switchRenderedType }) => (
+    <div className="ticketbox-success">
+        Well, this is embarassing.
             <br />
-            <div className="ticketbox-subtext">
-                We seem to have hit an error, <br /> so please try again later!
+        <div className="ticketbox-subtext">
+            We seem to have hit an error, <br /> so please try again later!
             </div>
-            <BackBtn type="error" switchRenderedType={switchRenderedType}/>
-        </div>
+        <BackBtn type="error" switchRenderedType={switchRenderedType} />
     </div>
 );
 
