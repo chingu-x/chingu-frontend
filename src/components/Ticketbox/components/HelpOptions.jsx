40e34da45a44b7369a781432a580515f8d98cbe3
serpient <littlegangwolf@gmail.com>
2018-09-10 01:47:07 -0700

    create helpOptions component
    - btns that take u to either team or other form

---
+++ src/components/Ticketbox/components/HelpOptions.jsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+
+const HelpOptions = ({ switchHelpType }) => {
+    let assets = [
+      'help-team.png',
+      'help-other.png'
+    ];
+    let labels = ['team', 'other'];
+    return (
+      <div className="help-btns--container">
+        {
+          labels.map((text, idx) => {
+            return (
+              <div key={idx} onClick={() => switchHelpType(text)} className="help-btns">
+                <img
+                  className="ticketbox-btn"
+                  alt="ticketbox-btn"
+                  src={require(`../../../assets/${assets[idx]}`)}
+                />
+                <div className="ticketbox-btn--text">{text}</div>
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
+
+  export default HelpOptions;
