2bc68ab29071b227ad9f21680862ed22a13c1923
serpient <littlegangwolf@gmail.com>
2018-09-10 15:51:12 -0700

    helpOther questions/answers

---
+++ src/components/Ticketbox/components/HelpOther.js
@@ -0,0 +1,12 @@
+export const QA = [
+    {
+        text: 'Title',
+        input_type: 'text',
+        field_name: 'title'
+    },
+    {
+        text: 'Body',
+        input_type: 'textarea',
+        field_name: 'body'
+    }
+];
