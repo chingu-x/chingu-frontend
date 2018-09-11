4390f79b9a85002a81464a33bcc5b47f3d5d65aa
serpient <littlegangwolf@gmail.com>
2018-09-10 20:12:23 -0700

    update types of questionComponents in index

--- src/components/DynamicForm/components/DynamicFormMaker/QuestionComponents/index.js
+++ src/components/DynamicForm/components/DynamicFormMaker/QuestionComponents/index.js
@@ -4,9 +4,13 @@ import dropdown from "./dropdown";
 import radio from "./radio";
 import text from "./text";
 import textarea from "./textarea";
+import date from './date';
 
 import team_progress_sentiment_buttons from "./team_progress_sentiment_buttons.jsx";
 import voyage_application_tier_select from "./voyage_application_tier_select";
+import three_buttons from './three_buttons';
+import dropdown_team_cards from './dropdown_team_cards';
+import dropdown_users from './dropdown_users';
 
 export default {
   checkbox,
@@ -15,7 +19,11 @@ export default {
   radio,
   text,
   textarea,
+  date,
 // -- CUSTOM -- //
   voyage_application_tier_select,
   team_progress_sentiment_buttons,
+  dropdown_team_cards,
+  three_buttons,
+  dropdown_users
 };
