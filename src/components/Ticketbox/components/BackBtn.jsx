e86d22f20cdc12b90def3a373a477eab64e49611
thinktwice13 <marioo.krajacic@gmail.com>
2018-09-11 16:31:47 +0200

    Add Back button to TicketBox HelpPage

--- src/components/Ticketbox/components/BackBtn.jsx
+++ src/components/Ticketbox/components/BackBtn.jsx
@@ -1,12 +1,19 @@
 import * as React from "react";
+import PropTypes from "prop-types"
 
-const BackBtn = ({ path, type, switchRenderedType, switchHelpType }) => (
+const BackBtn = ({
+  path,
+  type,
+  className,
+  switchRenderedType,
+  switchHelpType }) => (
     <input
-        type="button"
-        value="BACK"
-        className={type === 'error' ? "form-error-back-btn" : "form-back-btn"}
-        onClick={switchHelpType ? () => switchHelpType(path) : () => switchRenderedType(path)}
+      type="button"
+      value="BACK"
+      className={className || (type === 'error' ? "form-error-back-btn" : "form-back-btn")}
+      onClick={switchHelpType ? () => switchHelpType(path) : () => switchRenderedType(path)}
     />
-);
+  );
+
 
 export default BackBtn;
--- src/components/Ticketbox/components/Help/index.jsx
+++ src/components/Ticketbox/components/Help/index.jsx
@@ -4,6 +4,7 @@ import Error from '../Error';
 import HelpOptions from './HelpOptions';
 import HelpPageSearch from './HelpPageSearch';
 import TeamHelp from './TeamHelp';
+import BackBtn from "../BackBtn"
 
 class Help extends React.Component {
   state = { type: '', response: null, error: null }
@@ -23,18 +24,18 @@ class Help extends React.Component {
     let { response } = this.state;
     switch (type) {
       case 'team help':
-      // TODO: grey / disable this when the user is not on any teams?
+        // TODO: grey / disable this when the user is not on any teams?
         return <TeamHelp
-                  category="team"
-                  setResponse={this.setResponse}
-                  switchHelpType={this.switchHelpType} />
+          category="team"
+          setResponse={this.setResponse}
+          switchHelpType={this.switchHelpType} />
       case 'general':
         return <HelpPageSearch
-                  switchRenderedType={switchRenderedType}
-                  switchHelpType={this.switchHelpType} />
+          switchRenderedType={switchRenderedType}
+          switchHelpType={this.switchHelpType} />
       case 'error':
         return <Error
-                switchRenderedType={switchRenderedType} />
+          switchRenderedType={switchRenderedType} />
       case 'success': // TODO: help requests dont have an associated github issue
         return <Success category="help" url={response.github_issue.url} />
       default:
@@ -53,7 +54,7 @@ class Help extends React.Component {
           <img className="box-icon" alt="icon" src={imgSrc} />
         </div>
         {this.renderHelpSections(type)}
-        {/* TODO: back button to get back to ticketbox main view */}
+        <BackBtn className="form-btn" path={""} switchRenderedType={this.props.switchRenderedType} />
       </div>
     )
   }
