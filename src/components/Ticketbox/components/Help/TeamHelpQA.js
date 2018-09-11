d17006fa94a07b5e6c3ff4c25fecd4f9c0bbbadb
The-Vampiire <vampiirecodes@gmail.com>
2018-09-11 04:12:11 -0400

    refactored temporary data and shape for Question components in TeamHelp

---
+++ src/components/Ticketbox/components/Help/TeamHelpQA.js
@@ -0,0 +1,70 @@
+export const TeamHelpBaseQA = [
+  {
+    text: 'Team',
+    input_type: 'dropdown_team_cards',
+    field_name: 'team_id',
+    options: [
+      {
+        id: 1,
+        title: 'Voyage 5',
+        project: {
+          title: 'Chingu.io'
+        },
+        cohort_users: [
+          {
+            user: {
+              avatar: 'https://avatars0.githubusercontent.com/u/29721784?v=4',
+              username: 'serpient'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  {
+    text: 'Issue',
+    input_type: 'three_buttons',
+    field_name: 'request_subtype',
+    options: [
+      'inactivity',
+      'withdraw',
+      'other'
+    ]
+  }
+];
+
+export const ContextQA = {
+  text: 'Description of Issue',
+  input_type: 'textarea',
+  field_name: 'context'
+};
+
+export const InactivityQA = [
+  {
+    text: 'Team Member',
+    input_type: 'dropdown_users',
+    field_name: 'inactive_user_id',
+    options: [
+      {
+        id: 1,
+        avatar: 'https://avatars0.githubusercontent.com/u/29721784?v=4',
+        username: 'serpient'
+      },
+      {
+        id: 2,
+        avatar: 'https://avatars0.githubusercontent.com/u/29721784?v=4',
+        username: 'serpient'
+      },
+      {
+        id: 3,
+        avatar: 'https://avatars0.githubusercontent.com/u/29721784?v=4',
+        username: 'serpient'
+      }
+    ]
+  },
+  {
+    text: 'Last Contacted',
+    input_type: 'date',
+    field_name: 'last_contact'
+  },
+];
