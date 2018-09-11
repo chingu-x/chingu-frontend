83592f3d7a6a2205dee7f4e993483d884ff893c0
serpient <littlegangwolf@gmail.com>
2018-09-10 20:11:42 -0700

    add date form type to dynamicForms

---
+++ src/components/DynamicForm/components/DynamicFormMaker/QuestionComponents/date.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+// TODO: support 'date' type. confirm the format the date input returns
+// matches what is expected on the backend
+export default (
+  { input_type, field_name, minlength, maxlength },
+  onFormChange,
+  form_data,
+) => (
+  <input 
+    type={input_type}
+    name={field_name}
+    value={form_data[field_name]}
+    className="form-date"
+    onChange={
+      ({ currentTarget }) => onFormChange(
+        { currentTarget, min: minlength, max: maxlength }
+      )
+    }
+  />
+);
