ee520e7b56267aa10f78b8179cc3230a90cd3a42
The-Vampiire <vampiirecodes@gmail.com>
2018-09-11 21:00:58 -0400

    TeamHelp receive user as prop from Help. fixed date value.

--- src/components/Ticketbox/components/Help/TeamHelp.jsx
+++ src/components/Ticketbox/components/Help/TeamHelp.jsx
@@ -1,34 +1,10 @@
 import * as React from "react";
 import BackBtn from '../BackBtn';
 import { gql } from "apollo-boost";
-import { DynamicFormContainer, dynamicFormMaker } from "../../../DynamicForm";
+import { DynamicFormContainer } from "../../../DynamicForm";
 import { client } from "../../../../";
-import Request from "../../../utilities/Request"
-import Loader from "../../../Loader"
 import { TeamHelpBaseQA, InactivityQA, ContextQA } from './TeamHelpQA';
 
-const userActiveTeamsQuery = gql`
- query getUserActiveTeams {
-  user {
-    id
-    teams(only_active: true) {
-      id
-      title
-      project { 
-        id
-        title
-      }
-      cohort_users {
-        user {
-          id
-          username
-          avatar
-        }
-      }
-    }
-  }
- }
-`
 class TeamHelp extends React.Component {
   constructor(props) {
     super(props);
@@ -37,46 +13,118 @@ class TeamHelp extends React.Component {
 
     // sets the questions based on persisted data from DFContainer
     let requestSubtype = null;
-    let teamID = null;
+    let chosenTeamID = null;
     if (persistedData) {
       const { form_data: { request_subtype, team_id } } = JSON.parse(persistedData);
       requestSubtype = request_subtype;
-      teamID = team_id;
+      chosenTeamID = team_id;
     }
 
     this.state = {
       error: null,
       response: null,
       requestSubtype,
-      teamID,
+      chosenTeamID,
+      questions: this.getQuestions(requestSubtype, chosenTeamID),
     }
   }
 
-  getQuestions = (subtype, teams, teamID, currentUserID) => {
-    switch (subtype) {
+  getQuestions = (requestSubtype, chosenTeamID) => {
+    const userMock = {
+      user: {
+        id: 10,
+        teams: [
+          {
+            id: 1,
+            title: "Vampires Team 0",
+            project: {
+              id: 1,
+              title: "Chingu API",
+            },
+            cohort_users: [
+              {
+                user: {
+                  id: 1,
+                  username: "the-vampiire",
+                  avatar: "https://avatars2.githubusercontent.com/u/25523682?s=88&v=4",
+                }
+              },
+              {
+                user: {
+                  id: 2,
+                  username: "thinktwice13",
+                  avatar: "https://avatars2.githubusercontent.com/u/25523682?s=88&v=4",
+                }
+              },
+            ],
+          },
+          {
+            id: 2,
+            title: "Werewolves Team 0",
+            project: {
+              id: 2,
+              title: "Chingu Frontend",
+            },
+            cohort_users: [
+              {
+                user: {
+                  id: 3,
+                  username: "serpient",
+                  avatar: "https://avatars2.githubusercontent.com/u/25523682?s=88&v=4",
+                }
+              },
+              {
+                user: {
+                  id: 1,
+                  username: "the-vampiire",
+                  avatar: "https://avatars2.githubusercontent.com/u/25523682?s=88&v=4",
+                }
+              },
+            ]
+          },
+        ],
+      },
+    };
+    // TODO: replace with actual query
+    // const { user, user: { teams } } = this.props;
+    const { user, user: { teams } } = userMock;
+    
+    switch (requestSubtype) {
       case 'inactivity':
         return [
           ...TeamHelpBaseQA(teams),
-          ...InactivityQA(teams, teamID, currentUserID),
-          ContextQA,
+          ...InactivityQA(user, chosenTeamID),
+          ContextQA(requestSubtype),
         ];
       default:
-        return [...TeamHelpBaseQA(teams), ContextQA];
+        return [...TeamHelpBaseQA(teams), ContextQA(requestSubtype)];
     }
   };
 
   // passed as prop to DFContainer
   // provides access to the request_subtype value when it changes
-  handleInputChange = (fieldName, value) => {
+  handleInputChange = (fieldName, value, formData) => {
     // ignore other field names
     if (!['request_subtype', 'team_id'].includes(fieldName)) return;
 
     // update values when DFContainer input changes
+    let requestSubtype, chosenTeamID;
     if (fieldName === 'request_subtype') {
-      this.setState({ requestSubtype: value });
+      // update the chosen value from form
+      requestSubtype = value;
+      // get the current value for teamID from form
+        // handles initial case where teamID is initialized
+        // in DFContainer but not reflected in TeamHelp
+      chosenTeamID = formData.team_id;
     } else if (fieldName === 'team_id') {
-      this.setState({ teamID: value });
+      // change nothing
+      requestSubtype = this.state.requestSubtype;
+      // update teamID from input change value
+      chosenTeamID = value;
     };
+
+    const questions = this.getQuestions(requestSubtype, chosenTeamID);
+    this.setState({ requestSubtype, chosenTeamID, questions });
   }
 
   handleResponse = ({ data }) => {
@@ -86,18 +134,26 @@ class TeamHelp extends React.Component {
 
   handleError = error => this.setState({ error });
 
-  submitRequest = (help_request_data) => {
+  submitRequest = (form_data) => {
     const mutation = gql`
-      mutation submitTeamHelpRequest(
-        $help_request_data: TeamHelpRequestCreateInput!
-      ) {
-        teamHelpRequestCreate(
-          help_request_data: $help_request_data
+        mutation submitTeamHelpRequest(
+          $help_request_data: TeamHelpRequestCreateInput!
         ) {
-          id
+          teamHelpRequestCreate(
+            help_request_data: $help_request_data
+          ) {
+            id
+          }
         }
-      }
-    `;
+      `;
+
+    const help_request_data = { ...form_data };
+    
+    // convert to unix timestamp before submitting
+    if (form_data.last_contact) {
+      const formattedDate = Number(new Date(form_data.last_contact));
+      help_request_data.last_contact = formattedDate;
+    }
 
     const variables = { help_request_data };
     client.mutate({ mutation, variables })
@@ -106,82 +162,24 @@ class TeamHelp extends React.Component {
   }
 
   render() {
-    const { setResponse, switchHelpType, loading, data: { user } } = this.props;
-    const { error, response, requestSubtype, teamID } = this.state;
+    const { setResponse, switchHelpType } = this.props;
+    const { error, response, questions } = this.state;
 
     if (error) return switchHelpType('error');
+    
+    // TODO: setResponse is broken (should return a success component)
     if (response) return setResponse(response);
 
-    // TODO: remove once querying for actual user
-    const userMock = {
-      teams: [
-        {
-          id: 1,
-          title: "Vampires Team 0",
-          project: {
-            id: 1,
-            title: "Chingu API",
-          },
-          cohort_users: [
-            {
-              user: {
-                id: 1,
-                username: "the-vampiire",
-                avatar: "https://avatars2.githubusercontent.com/u/25523682?s=88&v=4",
-              }
-            },
-            {
-              user: {
-                id: 2,
-                username: "thinktwice13",
-                avatar: "https://avatars2.githubusercontent.com/u/25523682?s=88&v=4",
-              }
-            },
-          ],
-        },
-        {
-          id: 2,
-          title: "Werewolves Team 0",
-          project: {
-            id: 2,
-            title: "Chingu Frontend",
-          },
-          cohort_users: [
-            {
-              user: {
-                id: 3,
-                username: "serpient",
-                avatar: "https://avatars2.githubusercontent.com/u/25523682?s=88&v=4",
-              }
-            },
-            {
-              user: {
-                id: 1,
-                username: "the-vampiire",
-                avatar: "https://avatars2.githubusercontent.com/u/25523682?s=88&v=4",
-              }
-            },
-          ]
-        },
-      ],
-    };
-
-    // TODO: replace with user when querying for actual user
-    const questions = this.getQuestions(requestSubtype, userMock.teams, teamID, user.id);
     return (
       <div className="help-team-container">
         <div className="ticketbox-form">
-          {
-            loading
-              ? <Loader size="medium" height="478.6px" color="#E20000" />
-              : <DynamicFormContainer
-                purpose="team-help-request"
-                questions={questions}
-                onSubmit={this.submitRequest}
-                onInputChange={this.handleInputChange}
-                persistence
-              />
-          }
+          <DynamicFormContainer
+            purpose="team-help-request"
+            questions={questions}
+            onSubmit={this.submitRequest}
+            onInputChange={this.handleInputChange}
+            persistence
+          />
           <BackBtn path={"help-options"} type="left" switchHelpType={switchHelpType} />
         </div>
       </div>
@@ -189,11 +187,4 @@ class TeamHelp extends React.Component {
   }
 }
 
-// export default TeamHelp
-export default props =>
-  <Request
-    {...props}
-    query={userActiveTeamsQuery}
-    component={TeamHelp}
-  />
-
+export default TeamHelp
