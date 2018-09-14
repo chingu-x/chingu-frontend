ed108ce5f3f5733836eee2fca42a08b5db1d7538
The-Vampiire <vampiirecodes@gmail.com>
2018-09-13 23:53:00 -0400

    HelpOptions commented out help General search until next sprint

--- src/components/Ticketbox/components/Help/HelpOptions.jsx
+++ src/components/Ticketbox/components/Help/HelpOptions.jsx
@@ -1,15 +1,19 @@
 import * as React from "react";
 
 const HelpOptions = ({ switchHelpType, hasActiveTeams }) => {
-  const buttons = [{
-    text: "team help",
-    src: hasActiveTeams ? 'help-team.png' : 'help-team-disabled.png',
-    disabled: hasActiveTeams ? "" : " disabled"
-  }, {
-    text: "general",
-    src: "help-other.png",
-    disabled: ""
-  }]
+  const buttons = [
+    {
+      text: "team help",
+      src: hasActiveTeams ? 'help-team.png' : 'help-team-disabled.png',
+      disabled: hasActiveTeams ? "" : " disabled"
+    },
+    // TODO: uncomment when help section is refactored
+    // {
+    //   text: "general",
+    //   src: "help-other.png",
+    //   disabled: ""
+    // },
+  ];
 
   return (
     <div className="help-btns--container">
