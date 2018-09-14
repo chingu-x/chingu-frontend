7e5e86f3a4c93bc1e3cae53c5ce6711c1db04cd7
The-Vampiire <vampiirecodes@gmail.com>
2018-09-14 00:28:33 -0400

    merge BugSuggestion versions. simplify render logic. fix LS

--- src/components/Ticketbox/components/Feedback/BugSuggestion.jsx
+++ src/components/Ticketbox/components/Feedback/BugSuggestion.jsx
@@ -3,16 +3,17 @@ import PropTypes from "prop-types"
 import { DynamicFormContainer } from "../../../DynamicForm";
 import { gql } from "apollo-boost";
 import { client } from "../../../../";
-import { QA } from './BugSuggestionQA';
-import Success from '../Success';
-import TicketBoxError from '../TicketBoxError';
-import BackBtn from '../BackBtn';
+import { QA } from "./BugSuggestionQA";
+import Success from "../Success";
+import TicketBoxError from "../TicketBoxError";
+import BackBtn from "../BackBtn";
 
 class BugSuggestion extends React.Component {
   state = { error: null, response: null };
 
   handleResponse = ({ data }) => {
-    window.localStorage.removeItem("ticketbox");
+    const { category } = this.props;
+    window.localStorage.removeItem(`ticketbox-${category}`);
     this.setState({ response: data.feedbackCreate });
   }
 
@@ -42,40 +43,28 @@ class BugSuggestion extends React.Component {
     const imgFile = category === 'bug' ? 'Artboard 3-small.png' : 'Artboard 2-small.png';
     const imgSrc = require(`../../../../assets/${imgFile}`);
 
-    if (error) {
-      return (
-        <div className="bug-suggestion-box">
-          <div className={`box-color color--${category}`}>
-            <img className="box-icon" alt="icon" src={imgSrc} />
-          </div>
-          <TicketBoxError
-            switchRenderedType={switchRenderedType}
-          />
-        </div>
-      )
-    }
+    let component;
+    if (error) component = <TicketBoxError switchRenderedType={switchRenderedType} />;
+    else if (response) component = <Success category={category} url={response.github_issue.url} />;
+    else component = (
+      <div className="ticketbox-form">
+        <DynamicFormContainer
+          hiddenData={{ category }}
+          questions={QA(category)}
+          onSubmit={this.submitTicket}
+          persistence
+          purpose={`ticketbox-${category}`}
+        />
+        <BackBtn type="left" path={""} switchRenderedType={switchRenderedType} />
+      </div>
+    );
 
     return (
       <div className="bug-suggestion-box">
         <div className={`box-color color--${category}`}>
           <img className="box-icon" alt="icon" src={imgSrc} />
         </div>
-        {
-          response
-            ? <Success category={category} url={response.github_issue.url} />
-            : (
-              <div className="ticketbox-form">
-                <DynamicFormContainer
-                  hiddenData={{ category }}
-                  questions={QA(category)}
-                  onSubmit={this.submitTicket}
-                  persistence
-                  purpose={`ticketbox-${category}`}
-                />
-                <BackBtn type="left" path={""} switchRenderedType={switchRenderedType} />
-              </div>
-            )
-        }
+        {component}
       </div>
     )
   }
