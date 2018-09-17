82f41e6f4cad61212cf1839c084bc2c36d660e11
serpient <littlegangwolf@gmail.com>
2018-09-17 15:59:59 -0700

    remove popup modal from teamCard

--- src/components/FeedPortal/components/TeamCard.jsx
+++ src/components/FeedPortal/components/TeamCard.jsx
@@ -3,10 +3,7 @@ import { Link } from "react-router-dom"
 import './TeamCard.css';
 import InfoComponents from './InfoComponents';
 import TeamLinks from './TeamLinks';
-import PopupMenu from '../../utilities/PopupMenu';
-import { DynamicFormContainer } from "../../DynamicForm";
-import { gql } from "apollo-boost";
-import { client } from "../../../"; 
+
 
 const TeamCard = ({ user: { available_standups }, team }) => {
   let editorIsVisible = false;
@@ -46,53 +43,11 @@ const TeamCard = ({ user: { available_standups }, team }) => {
             src={require(`../../../assets/links.png`)}
             onClick={() => toggleEditorVisibility()}
           />
-          { editorIsVisible && <TeamLinksEditor /> }
           { team.project && <TeamLinks project={team.project} />}
         </div>
         
       </div>
     </div >
-  )
-}
-
-const questions = [
-  {
-    field_name: "github_url",
-    input_type: "text",
-    text: "Github Link"
-  },
-  {
-    field_name: "project_url",
-    input_type: "text",
-    text: "Live Project Link"
-  },
-  {
-    field_name: "communication_url",
-    input_type: "text",
-    text: "Communication Link",
-    subtext: "Ex. Slack, Discord"
-  },
-  {
-    field_name: "workflow_url",
-    input_type: "text",
-    text: "Workflow Link",
-    subtext: "Ex. Trello, Waffle"
-  }
-]
-
-const TeamLinksEditor = () => {
-  const onSubmit = () => {
-    //
-  }
-  return (
-    <PopupMenu className="team-links-editor-popup">
-      <div className="team-links-editor-container">
-        <DynamicFormContainer
-          questions={questions}
-          onSubmit={() => onSubmit()}
-        />
-      </div>
-    </PopupMenu>
   )
 }
 
