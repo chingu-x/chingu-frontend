c97b490aec4549472ae3519afd1008295411f899
serpient <littlegangwolf@gmail.com>
2018-09-17 16:00:10 -0700

    clean up teamLinks

--- src/components/FeedPortal/components/TeamLinks.jsx
+++ src/components/FeedPortal/components/TeamLinks.jsx
@@ -2,19 +2,18 @@ import * as React from 'react';
 
 const TeamLinks = ({ project: { github_url, project_url, communication_url, workflow_url } }) => {
     let key = ['github_url', 'project_url', 'communication_url', 'workflow_url'];
-    [github_url, project_url, communication_url, workflow_url].map((link, idx) => {
-        console.log(key[idx]);
+    let links = [github_url, project_url, communication_url, workflow_url].map((link, idx) => {
         return link &&
-            <img
-                key={idx}
-                alt="icon"
-                className="team-resource-links"
-                src={require(`../../../assets/${key[idx]}.png`)}
-            >
-                <a target="_blank" href={link} />
-            </img>
+            <a key={idx} target="_blank" href={link}>
+                <img
+                    key={idx}
+                    alt="icon"
+                    className="team-resource-links"
+                    src={require(`../../../assets/${key[idx]}.png`)}
+                />
+            </a>
     })
-    return null;
+    return links || null;
 }
 
 export default TeamLinks;
