88df8e24cc17c79ae283ea9a37f5ebe88c5972cd
serpient <littlegangwolf@gmail.com>
2018-09-26 13:39:37 -0700

    setup UserSkills section in user side bar

--- src/components/UserProfile/UserSideBar.jsx
+++ src/components/UserProfile/UserSideBar.jsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import './UserSideBar.css'
 import EditableTextField from '../utilities/EditableTextField';
 import { gql } from "apollo-boost";
+import SkillUpdater from './SkillUpdater';
 
 // -- MUTATION -- //
 const userUpdate = gql`
@@ -60,6 +61,64 @@ const UserInfo = ({ user, editable }) => {
   });
 }
 
+// -- MUTATION -- //
+const userAddSkills = gql`
+mutation userAddSkills($skill_ids: [ID!]) {
+  userAddSkills(skill_ids: $skill_ids) {
+    id
+    skills {
+      name
+      category
+    }
+  }
+}
+`;
+
+const userAddDesiredSkills = gql`
+mutation userAddDesiredSkills($skill_ids: [ID!]) {
+  userAddDesiredSkills(skill_ids: $skill_ids) {
+    id
+    skills {
+      name
+      category
+    }
+  }
+}
+`;
+
+
+// -- SKILL ELEMENTS -- //
+const USER_SKILLS_DOM_ELEMENTS = [
+  {
+    divClassName: 'user-skills',
+    schemaKey: 'skills',
+    desc: 'Skills',
+  },
+  {
+    divClassName: 'user-desired-skills',
+    schemaKey: 'desired_skills',
+    desc: 'Desired Skills',
+  },
+];
+
+const UserSkills = ({ user, editable }) => {
+  return USER_SKILLS_DOM_ELEMENTS.map((elem, idx) => {
+    const UserSkillComponent = props =>
+      <div key={idx} className={elem.divClassName}>
+        <h1 className="user-sidebar-subcategory">{elem.desc}</h1>
+        {props.children.map(elem => console.log(elem) (
+          <li>{elem}</li>
+        ))}
+      </div>
+    
+    const QA = []
+
+    return !editable // only render EditableTextField if editable
+      ? <UserSkillComponent key={idx}>{user[elem.schemaKey]}</UserSkillComponent>
+      : <SkillUpdater />
+  });
+}
+
 const Links = ({ user: { username } }) => (
   <div className="user-links">
     <h1 className="user-sidebar-subcategory">links</h1>
@@ -72,6 +131,7 @@ const Links = ({ user: { username } }) => (
     </ul>
   </div>
 );
+
 
 
 // -- USER SIDEBAR (EXPORT)-- // 
@@ -94,78 +154,12 @@ const UserSideBar = ({ user, editable }) => (
       </ul>
     </header>
     <UserInfo user={user} editable={editable} />
+    <UserSkills user={user} editable={editable} />
     <Links user={user} />
   </div>
 );
 
-// class UserSideBar extends React.Component {
-//   state = {
-//     user: null,
-//     editable: false,
-//   }
-
-//   updateState = () => {
-//     this.setState({
-//       user,
-//       editable
-//     });
-//   }
-//   componentDidMount() {
-//     this.updateState();
-//   }
-
-//   componentDidUpdate(prevProps) {
-//     if (
-//       this.props.editable !== prevProps.editable
-//       || this.props.user !== prevProps.user
-//     ) {
-//       this.updateState();
-//     }
-//   }
-
-//   render() {
-//     const { user, editable } = this.state;
-//     if (!user) return null;
-//     return (
-//       <div className="user-profile-container-personal">
-//         <header className="user">
-//           <div className="photobox">
-//             <img
-//               className="user-photo"
-//               src={user ? user.avatar : require('../../assets/blank image.png')}
-//               alt="userprofile"
-//             />
-//             <p>{user.username}</p>
-//             <p>Based in {user.country}</p>
-//           </div>
-//           <ul className="positions">
-//             <li className="position">
-//               <span>
-//                 <i className="fas fa-check" />
-//               </span>Programmer
-//               </li>
-//           </ul>
-//         </header>
-//         <UserInfo user={user} editable={editable} />
-//         <Links user={user} />
-//       </div>
-//     );
-//   }
-// }
-
 export default UserSideBar;
-
-// let skillDOM = null;
-// if (user.skills && user.skills.length > 0) {
-//   skillDOM = (
-//     <div className="user-skills">
-//       <h1 className="user-sidebar-subcategory">skills</h1>
-//       <ul>
-//         {user.skills.map(elem => (<li>{elem}</li>))}
-//       </ul>
-//     </div>
-//   )
-// }
 
 // once links are integrated again, render this
 // check for fb/linkedin/gitub
