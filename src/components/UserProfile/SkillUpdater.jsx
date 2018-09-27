403dc37ca80b14f20fb4f707c05b8b46d5abda30
serpient <littlegangwolf@gmail.com>
2018-09-27 16:09:43 -0700

    remove dummy skill data and hookup to query results

--- src/components/UserProfile/SkillUpdater.jsx
+++ src/components/UserProfile/SkillUpdater.jsx
@@ -11,85 +11,6 @@ import skillQuery from './graphql/skillQuery';
  */
 
 
-const SkillResults = [
-    {
-        frontend: [
-            {
-                id: 1,
-                name: 'React',
-                category: 'frontend'
-            },
-            {
-                id: 2,
-                name: 'Angular',
-                category: 'frontend'
-            },
-            {
-                id: 3,
-                name: 'HTML/CSS',
-                category: 'frontend'
-            }
-        ],
-        frontend_dependency: [
-            {
-                id: 1,
-                name: 'Redux',
-                category: 'frontend_dependency'
-            },
-            {
-                id: 2,
-                name: 'React-Router',
-                category: 'frontend_dependency'
-            },
-            {
-                id: 2,
-                name: 'SCSS',
-                category: 'frontend_dependency'
-            }
-        ],
-        database: [
-            {
-                id: 1,
-                name: 'MongoDB',
-                category: 'database'
-            },
-            {
-                id: 2,
-                name: 'SQL',
-                category: 'database'
-            }
-        ],
-        backend: [
-            {
-                id: 1,
-                name: 'ExpressJS',
-                category: 'backend'
-            }
-        ],
-        backend_dependency: [
-            {
-                id: 1,
-                name: 'Mongoose',
-                category: 'backend_dependency'
-            },
-            {
-                id: 2,
-                name: 'Graphql',
-                category: 'backend_dependency'
-            }
-        ]
-    }
-]
-
-const QA = [
-    {
-        text: 'Your Skills',
-        input_type: 'skill_setter',
-        field_name: 'skills',
-        options: SkillResults
-    }
-]
-
 const DesiredSkillQA = [
     {
         text: 'Your Desired Skills',
@@ -98,7 +19,15 @@ const DesiredSkillQA = [
     }
 ]
 
-const SkillUpdater = () => {
+const SkillUpdater = ({ data }) => {
+    const QA = [
+        {
+            text: 'Your Skills',
+            input_type: 'skill_setter',
+            field_name: 'skills',
+            options: [data]
+        }
+    ]
     return (
         <PopupMenu>
             <button>Open Skill Updater</button>
