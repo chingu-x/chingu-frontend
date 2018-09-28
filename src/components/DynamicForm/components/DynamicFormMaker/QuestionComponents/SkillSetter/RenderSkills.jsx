7e310ad88f36b789bf82f294bafbbc7a8aa8a1a3
serpient <littlegangwolf@gmail.com>
2018-09-27 19:46:44 -0700

    extract renderSkills

---
+++ src/components/DynamicForm/components/DynamicFormMaker/QuestionComponents/SkillSetter/RenderSkills.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import SourceSkillCard from './SourceSkillCard';
+
+const RenderSkills = ({ SKILL_ARRAY, connectDragSource, isDragging }) => {
+    console.log(connectDragSource)
+    return SKILL_ARRAY.map((category, idx) => {
+        return (
+            <div key={idx} className="skill-subcategory">
+                <div className="skill-subcategory--title">{category[0].category}</div>
+                {
+                    category.map((skill, idx) => {
+                        return <SourceSkillCard skill={skill} key={idx} />
+                    })
+                }
+            </div>
+        )
+    })
+}
+
+export default RenderSkills;
