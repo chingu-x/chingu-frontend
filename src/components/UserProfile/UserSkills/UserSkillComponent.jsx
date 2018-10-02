d32a0e640f28114552bfd9deb5ab0684f6aefbdc
serpient <littlegangwolf@gmail.com>
2018-10-01 20:04:52 -0700

    update userSkillComponent to render skill correctly

--- src/components/UserProfile/UserSkills/UserSkillComponent.jsx
+++ src/components/UserProfile/UserSkills/UserSkillComponent.jsx
@@ -4,9 +4,9 @@ const UserSkillComponent = ({ skills, elem }) => {
     return (
         <div className={elem.divClassName}>
             <h1 className="user-sidebar-subcategory">{elem.desc}</h1>
-            {skills.map(elem => (
-                <li>{elem}</li>
-            ))}
+            {skills.map((elem, idx) => {
+                return <li className={'skill-' + elem.category} key={idx}>{elem.name}</li>
+            })}
         </div>
     )
 }
