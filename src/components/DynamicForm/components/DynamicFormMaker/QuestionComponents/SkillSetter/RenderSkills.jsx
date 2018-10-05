cd2d04aa1aed9f1421a039181b031ea205e053f9
serpient <littlegangwolf@gmail.com>
2018-10-05 11:33:10 -0700

    set renderSkills component as dropTarget

--- src/components/DynamicForm/components/DynamicFormMaker/QuestionComponents/SkillSetter/RenderSkills.jsx
+++ src/components/DynamicForm/components/DynamicFormMaker/QuestionComponents/SkillSetter/RenderSkills.jsx
@@ -1,19 +1,42 @@
 import React from "react";
 import SourceSkillCard from './SourceSkillCard';
+import { ItemTypes } from './ItemTypes';
+import { DropTarget } from 'react-dnd';
 
-const RenderSkills = ({ SKILL_ARRAY }) => {
-    return SKILL_ARRAY.map((category, idx) => {
-        return (
-            <div key={idx} className="skill-subcategory">
-                <div className="skill-subcategory--title">{category[0].category}</div>
-                {
-                    category.map((skill, idx) => {
-                        return <SourceSkillCard skill={skill} key={idx} />
-                    })
-                }
-            </div>
-        )
-    })
+const skillTarget = {
+    drop(props, monitor) {
+        return {
+            position: props.position,
+            skill: monitor.getItem().skillId,
+            removeSkillHandler: props.removeSkillHandler
+        }
+    }
 }
 
-export default RenderSkills;
+function collect(connect, monitor) {
+    return {
+        connectDropTarget: connect.dropTarget(),
+        isOver: monitor.isOver()
+    }
+}
+
+const RenderSkills = ({ SKILL_ARRAY, connectDropTarget, removeSkillHandler }) => {
+    return connectDropTarget(
+        <div>
+            {SKILL_ARRAY.map((category, idx) => {
+                return (
+                    <div key={idx} className="skill-subcategory">
+                        <div className="skill-subcategory--title">{category[0].category}</div>
+                        {
+                            category.map((skill, idx) => {
+                                return <SourceSkillCard skill={skill} key={idx} />
+                            })
+                        }
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default DropTarget(ItemTypes.SKILL_CARD, skillTarget, collect)(RenderSkills);
