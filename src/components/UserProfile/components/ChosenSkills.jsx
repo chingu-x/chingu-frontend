45a8c1625a4ab340edaa96f897595f41d3d65119
The-Vampiire <vampiirecodes@gmail.com>
2018-10-14 00:42:52 -0400

    added notes to ChosenSkills for rendering requested skills

--- src/components/UserProfile/components/ChosenSkills.jsx
+++ src/components/UserProfile/components/ChosenSkills.jsx
@@ -1,6 +1,9 @@
 import * as React from "react";
 
 const ChosenSkills = ({ skills, description }) => {
+  const requestedSkills = localStorage.getItem('userRequestedSkills');
+  // TODO: handle rendering requested skills
+  // TODO: need greyed out class and tooltip
   return (
     <div className="chosen-skills">
       <h1 className="sidebar-subcategory">{description}</h1>
