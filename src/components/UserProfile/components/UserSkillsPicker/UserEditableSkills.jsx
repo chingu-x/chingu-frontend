abd36087bc6cffd51b4d63c6d18bcd2b05bf5304
The-Vampiire <vampiirecodes@gmail.com>
2018-10-14 20:25:22 -0400

    UserEditableSkills handle new requested skills. TODO: css

--- src/components/UserProfile/components/UserSkillsPicker/UserEditableSkills.jsx
+++ src/components/UserProfile/components/UserSkillsPicker/UserEditableSkills.jsx
@@ -1,11 +1,35 @@
 import React from "react";
+import { gql } from "apollo-boost";
 import ChosenSkills from "../ChosenSkills";
 import SkillsPicker from "../../../SkillsPicker";
 
+const userAddSkills = gql`
+  mutation userAddSkills ($skill_ids:[ID!]!) {
+    userAddSkills(skill_ids:$skill_ids) {
+      id
+      skills {
+        id
+        name
+        category
+      }
+      requested_skills {
+        id
+        name
+      }
+    }
+  }
+`;
+// TODO: create 'user-editable-skills' class, remove style
 const UserEditableSkills = ({ user }) => (
   <div className="user-editable-skills" style={{ "margin": 50 }}>
-    <ChosenSkills skills={user.skills} description="Acquired Skills" />
-    <SkillsPicker currentSkills={user.skills} />
+    <ChosenSkills
+      description="Acquired Skills" 
+      skills={[...user.skills, ...user.requested_skills]}
+    />
+    <SkillsPicker
+      mutation={userAddSkills}
+      currentSkills={user.skills}
+    />
   </div>
 );
 
