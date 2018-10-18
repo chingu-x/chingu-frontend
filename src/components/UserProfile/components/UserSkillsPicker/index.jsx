32efb6a34c5aef0f5bc193f790933183619b8472
thinktwice13 <marioo.krajacic@gmail.com>
2018-10-18 16:16:21 +0200

    Change user.skills into user.acquired_skills

--- src/components/UserProfile/components/UserSkillsPicker/index.jsx
+++ src/components/UserProfile/components/UserSkillsPicker/index.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+import PropTypes from 'prop-types'
 import ChosenSkills from "../ChosenSkills";
 import UserEditableSkills from "./UserEditableSkills";
 
 const UserSkillsPicker = ({ user, editable }) => (
-  editable
+  console.log({user})
     ? <UserEditableSkills user={user} />
     : <ChosenSkills
         description="Acquired Skills"
-        skills={[...user.skills, ...user.requested_skills]}
+        skills={[...user.acquired_skills, ...user.requested_skills]}  
       />
 );
+
+UserSkillsPicker.defaultProps = {
+  skills: [],
+  requested_skills: []
+}
 
 export default UserSkillsPicker;
