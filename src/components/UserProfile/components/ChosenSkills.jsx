d43ac86c8542420296b9dbb34d00b785d4da0a48
thinktwice13 <marioo.krajacic@gmail.com>
2018-10-19 15:37:00 +0200

    Remove unnecessary state in DesiredSkills

--- src/components/UserProfile/components/ChosenSkills.jsx
+++ src/components/UserProfile/components/ChosenSkills.jsx
@@ -11,20 +11,21 @@ const SkillTag = ({ skill, className, isAcquiredSkill }) => (
 );
 
 const mapSkills = skills => skills.map((skill, idx) => {
-  // only acquired skills have showcase_count property
-  const isAcquiredSkill = skill.hasOwnProperty('showcase_count');
-  // only acquired and desired skills have category property
-  const isDesiredSkill = skill.hasOwnProperty('category');
-  const className = (isAcquiredSkill || isDesiredSkill)
-    ? `skill-${skill.category}`
-    : "skill-requested";
+  // // only acquired skills have showcase_count property
+  // const isAcquiredSkill = skill.hasOwnProperty('showcase_count');
+  // // only acquired and desired skills have category property
+  // const isDesiredSkill = skill.hasOwnProperty('category');
+  // const className = (isAcquiredSkill || isDesiredSkill)
+  //   ? `skill-${skill.category}`
+  //   : "skill-requested";
+  const className = !skill.category ? 'skill-requested' : `skill-${skill.category}`
 
   return (
     <SkillTag
       skill={skill}
       key={idx}
       className={className}
-      isAcquiredSkill={isAcquiredSkill}
+      isAcquiredSkill={skill.hasOwnProperty('showcase_count')}
     />
   ); 
 });
--- src/components/UserProfile/components/UserDesiredSkills/EditableDesiredSkills.jsx
+++ src/components/UserProfile/components/UserDesiredSkills/EditableDesiredSkills.jsx
@@ -2,28 +2,14 @@ import * as React from "react";
 import DesiredSkillsPicker from './DesiredSkillsPicker';
 import ChosenSkills from "../ChosenSkills";
 
-class EditableSkills extends React.Component {
-  state = {
-    updatedSkills: null
-  }
-// TODO: not needed
-  updateSkills = (data) => {
-    const { userAddDesiredSkills: { desired_skills } } = data;
-    return this.setState({ updatedSkills: desired_skills });
-  }
+const EditableSkills = ({ skills }) =>
+  <div>
+    <DesiredSkillsPicker />
+    <ChosenSkills
+      description="Desired Skills"
+      skills={skills}
+    />
+  </div>
 
-  render() {
-    const { skills } = this.props;
-    return (
-      <div>
-        <DesiredSkillsPicker updateSkills={this.updateSkills} />
-        <ChosenSkills
-          description="Desired Skills"
-          skills={this.state.updatedSkills || skills}
-        />
-      </div>
-    );
-  }
-}
 
 export default EditableSkills;
