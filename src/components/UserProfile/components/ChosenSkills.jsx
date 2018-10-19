be4f4b44b2cce05792ac65d2ab4f4ed92ed00ca8
The-Vampiire <vampiirecodes@gmail.com>
2018-10-18 20:00:42 -0400

    ChosenSkills: create SkillTag. refactor to support desired skills

--- src/components/UserProfile/components/ChosenSkills.jsx
+++ src/components/UserProfile/components/ChosenSkills.jsx
@@ -1,28 +1,44 @@
 import * as React from "react";
-// TODO: create css classes
+
+const SkillTag = ({ skill, idx, className, isAcquiredSkill }) => (
+  <li key={idx} className={className}>
+    <div>{skill.name}</div>
+    {/* {
+      isAcquiredSkill &&
+      <div className="skill-showcase-count">{skill.showcase_count}</div>
+    } */}
+  </li>
+);
+
 const mapSkills = skills => skills.map((skill, idx) => {
-  // requested skills have no category property
-  const isAcquiredSkill = !!skill.category;
-  const className = isAcquiredSkill ? `skill-${skill.category}` : "requested-skill";
+  // only acquired skills have showcase_count property
+  const isAcquiredSkill = skill.hasOwnProperty('showcase_count');
+  // only acquired and desired skills have category property
+  const isDesiredSkill = skill.hasOwnProperty('category');
+  const className = (isAcquiredSkill || isDesiredSkill)
+    ? `skill-${skill.category}`
+    : "skill-requested";
+
   return (
-    <li key={idx} className={className}>
-      <div className="acquired-skill-name">{skill.name}</div>
-      {
-        isAcquiredSkill &&
-        <div className="skill-showcase-count">{skill.showcase_count}</div>
-      }
-    </li>
-  );
+    <SkillTag
+      skill={skill}
+      idx={idx}
+      className={className}
+      isAcquiredSkill={isAcquiredSkill}
+    />
+  ); 
 });
 
 const ChosenSkills = ({
+  skills,
   description,
-  skills, // user.acquired_skills or acquired skills + requested skills
-}) => (
-  <div className="chosen-skills">
-    <h1 className="sidebar-subcategory">{description}</h1>
-    {mapSkills(skills)}
-  </div>
-);
+}) => {
+  return (
+    <div className="user-skills">
+      <h1 className="sidebar-subcategory">{description}</h1>
+      {mapSkills(skills)}
+    </div>
+  );
+}
 
 export default ChosenSkills;
