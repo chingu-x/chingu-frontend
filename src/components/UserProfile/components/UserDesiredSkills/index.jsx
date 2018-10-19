cbf36eb7003ccacedcd3d1e01d99fb105a76588f
thinktwice13 <marioo.krajacic@gmail.com>
2018-10-19 16:00:36 +0200

    Remove EditableDesiredSkills wrapper

--- src/components/UserProfile/components/UserDesiredSkills/index.jsx
+++ src/components/UserProfile/components/UserDesiredSkills/index.jsx
@@ -1,20 +1,20 @@
 import * as React from "react";
-import EditableDesiredSkills from "./EditableDesiredSkills";
 import ChosenSkills from "../ChosenSkills";
+import DesiredSkillsPicker from './DesiredSkillsPicker';
 import "./DesiredSkillPicker.css"
 /**
  * @prop {string} user  user object
  * @prop {string} editable whether the viewing user can edit
 */
-const UserDesiredSkills = ({ user, editable }) => (
-  editable
-    ? <EditableDesiredSkills skills={user.desired_skills} />
-    : (
-      <ChosenSkills
-        skills={user.desired_skills}
-        description="Desired Skills"
-      />
-    )
-);
+
+const UserDesiredSkills = ({ editable, user }) =>
+  <div>
+    {editable && <DesiredSkillsPicker />}
+    <ChosenSkills
+      description="Desired Skills"
+      skills={user.desired_skills}
+    />
+  </div>
+
 
 export default UserDesiredSkills;
