cb4cde31f0791df51329a7c2e19858e345fc2365
The-Vampiire <vampiirecodes@gmail.com>
2018-10-20 22:11:00 -0400

    PendingVoyages, CurrentVoyages: refactor to use voyage prop

--- src/components/UserProfile/ProjectCards/PendingVoyages.jsx
+++ src/components/UserProfile/ProjectCards/PendingVoyages.jsx
@@ -2,19 +2,16 @@ import React, { Fragment } from "react";
 import * as Cards from "../../VoyageCard/VoyageCard";
 
 const PendingApproval = ({ pendingApproval }) => (
-    <Fragment>
-        <div className="user-voyage-title">Pending Voyages</div>
-        {
-            pendingApproval.map((cohort, index) =>
-                <Cards.PendingApprovalVoyageCard
-                    key={cohort.id + "_" + index}
-                    voyageNumber={cohort.id}
-                    startDate={cohort.start_date}
-                    endDate={cohort.end_date}
-                    cohort={cohort.title}
-                />
-            )}
-    </Fragment>
+  <Fragment>
+    <div className="user-voyage-title">Pending Voyages</div>
+    {
+      pendingApproval.map((cohort, index) =>
+        <Cards.PendingApprovalVoyageCard
+          key={cohort.id + "_" + index}
+          voyage={cohort}
+        />
+      )}
+  </Fragment>
 )
 
 export default PendingApproval;
