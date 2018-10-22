b75de1912cfa293f4e729b0d34e234d99443ecc2
The-Vampiire <vampiirecodes@gmail.com>
2018-10-21 22:31:47 -0400

    removed old comments

--- src/components/UserProfile/components/UserAcquiredSkills/index.jsx
+++ src/components/UserProfile/components/UserAcquiredSkills/index.jsx
@@ -25,32 +25,12 @@ const userAddSkills = gql`
 class UserAcquiredSkills extends React.Component {
   state = {
     isEditing: false, // used to control rendering of SkillsPicker
-    // shouldSave: false, // used to control form submit in SkillsPicker
   }
 
   toggleEdit = () => this.setState(
     this.setState({ isEditing: !this.state.isEditing })
   );
 
-  /**
-   * Flow: TODO: Fixme
-   * 
-   * 1) Edit, ChosenSkills, SkillsPicker are mounted, state - initial
-   *     - Edit, ChosenSkills are rendered
-   *     - SkillsPicker -> componentDidMount -> sets isEditing: false -> renders null
-   * 2) Edit button clicked -> toggleEdit() -> isEditing: toggled true, shouldSave: no change, false
-   *     - SkillsPicker receives new props -> isEditing: true, shouldSave: false -> renders SkillsPicker
-   *     - ActionButtons rendered
-   * 3, cancel) User hits Cancel ActionButton -> toggleEdit() called
-   *     - isEditing: true -> toggled to false -> shouldSave: false, no change
-   *     - SkillsPicker -> new props -> isEditing: false, shouldSave: false -> renders null, still mounted
-   * 3, save) User hits Update ActionButton -> toggleSave() called
-   *     - shouldSave toggled true
-   *     - isEditing true -> this.toggleEdit() called
-   *     - SkillsPicker new props -> shouldSave true -> calls SkillsPicker.handleSubmit()
-   *     - UEditableSkills state updates -> isEditing: false, shouldSave: false,
-   *     - isEditing: false -> SkillsPicker unmounts
-   */
   render() {
     const { editable, user } = this.props;
     return (
