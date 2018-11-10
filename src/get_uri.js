0570692b1ca44172740878190e4149424e0a7cb4
thinktwice13 <marioo.krajacic@gmail.com>
2018-11-10 01:06:42 +0100

    Set staging and local npm scripts

--- src/get_uri.js
+++ src/get_uri.js
@@ -1,16 +1,16 @@
 const getURI = (mode) => {
-  switch(mode) {
+  switch (mode) {
     case 'local':
       return { uri: 'http://localhost:3500/graphql', clientID: "84a3576a59110d11cd6f" };
-    case 'production':
-      return  { uri: 'https://main-api.chingu.io/graphql', clientID: "e015fd9cc874fa5a34bf" };
     case 'staging':
-    default: 
       return { uri: 'https://main-api-staging.chingu.io/graphql', clientID: "84a3576a59110d11cd6f" };
+    case 'production':
+    default:
+      return { uri: 'https://main-api.chingu.io/graphql', clientID: "e015fd9cc874fa5a34bf" };
   }
 }
-// TODO: change this mode as needed. NEVER commit unless set to 'production'
-const mode = 'production';
+
+const mode = process.env.REACT_APP_API;
 const { uri, clientID } = getURI(mode);
-export { mode, clientID };
-export default uri;
+
+module.exports = { uri, clientID };
