064d31ec3daaf5621b86a0f77498ca5888850c25
serpient <littlegangwolf@gmail.com>
2018-11-26 01:17:15 -0800

    add projectSections mapping

--- src/components/UserProfile/index.jsx
+++ src/components/UserProfile/index.jsx
@@ -4,20 +4,34 @@ import Request from "../utilities/Request"
 import profileQuery from "./graphql/profileQuery"
 import './UserProfile.css'
 import PendingApproval from './ProjectCards/PendingVoyages';
-import ProjectCards from './ProjectCards/ProjectCards';
-
+import * as ProjectCards from '../ProjectCard';
 
 // -- USER PROFILE (EXPORT) -- //
 const UserProfile = ({ data, match }) => {
   // Only allow editing if no /profile param provided. TODO: Check for currently logged in user
   const editable = !match.params.username
 
-  const { user, user: { cohorts, projects } } = data; // Fetched user
-  // const pastTeams = projects.filter(team => team.cohort.status === 'ended');
-  // const currentTeams = projects.filter(team => team.cohort.status === 'ongoing');
+  const { user, user: { cohorts, projects, active_cohort_project, active_projects } } = data;
   const pendingApproval = cohorts.filter(cohort =>
     cohort.members.some(member => member.member_status !== "applicant"),
   );
+  const projectSections = [
+    {
+      sectionTitle: 'Current Cohort Project',
+      data: active_cohort_project,
+      cardComponent: <ProjectCards.CohortProjectCard project={active_cohort_project} />,
+    },
+    {
+      sectionTitle: 'Current Projects',
+      data: active_projects,
+      cardComponent: <ProjectCards.ProjectCard project={active_projects} />,
+    },
+    {
+      sectionTitle: 'All Other Projects',
+      data: projects,
+      cardComponent: <ProjectCards.ProjectCard project={projects} />,
+    },
+  ]
 
   return (
     <div className="user-profile-background-color">
@@ -27,21 +41,24 @@ const UserProfile = ({ data, match }) => {
         </aside>
 
         <main className="user-voyages">
-
           <section className="user-voyage">
             {
               pendingApproval.length > 0
               && <PendingApproval pendingApproval={pendingApproval} />
             }
           </section>
+          {projectSections.map((projectSection, idx) => {
+              const projectCard = !projectSection.data || projectSection.data.length === 0
+                ? <ProjectCards.NoProjectCard />
+                : projectSection.cardComponent;
 
-          <section className="user-voyage">
-            {!!projects.length && <div className="user-voyage-title">Current Projects</div>}
-            <ProjectCards projectsList={projects} />
-            {/* {!!pastTeams.length && <div className="user-voyage-title">Past Projects</div>}
-            <ProjectCards teamsList={pastTeams} /> */}
-          </section>
-
+              return (
+                <section className="user-voyage" key={idx}>
+                  <div className="user-voyage-title">{projectSection.sectionTitle}</div>
+                  {projectCard}
+                </section>
+              )
+          })}
         </main>
       </div>
     </div >
