0f890371d8730d0ea582ec5b82940b28d19454ab
serpient <littlegangwolf@gmail.com>
2018-11-26 01:17:54 -0800

    add default projectImage

--- src/components/ProjectCard/ProjectImage.jsx
+++ src/components/ProjectCard/ProjectImage.jsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 import { Link } from "react-router-dom"
 
-export const ProjectImage = ({ project }) => {
+const ProjectImage = ({ project }) => {
   return (
     <Link className="project-img" to={`/project/${project.id}`}>
       <img
         className="project-img"
-        src={require('../../../public/favicon/favicon-96x96.png')} />
+        src={require('./defaultProjectImg.png')} />
     </Link>
   )
 }
+
+export default ProjectImage;
