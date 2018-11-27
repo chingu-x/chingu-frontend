72ca8f12c1b7e45cd5b969ec44d2da5cb5286a51
serpient <littlegangwolf@gmail.com>
2018-11-27 01:44:15 -0800

    basic multiple_text_input component

--- src/components/DynamicForm/components/DynamicFormMaker/QuestionComponents/multiple_text_input.jsx
+++ src/components/DynamicForm/components/DynamicFormMaker/QuestionComponents/multiple_text_input.jsx
@@ -4,7 +4,7 @@ import text from "./text";
 const multiple_text_input = (data, onFormChange, form_data) => {
   console.log({ data, onFormChange, form_data })
   return (
-    <div className="btn-container--3">
+    <div className="multiple-input-container">
       {data.options.map((answer, index) => {
         return;
       })}
