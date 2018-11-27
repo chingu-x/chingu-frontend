63bc31e6ea2ca2d41d7594e1a460658549cc3b4d
serpient <littlegangwolf@gmail.com>
2018-11-26 16:07:07 -0800

    starter teamResourceLink component

---
+++ src/components/ProjectCard/TeamResourceLinks.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Link } from "react-router-dom"
+
+const TeamResourceLinks = ({ project }) => {
+  return (
+    <React.Fragment>
+
+    </React.Fragment>
+  )
+}
+
+export default TeamResourceLinks;
