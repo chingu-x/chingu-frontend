efc034b9aa62535c69783835b2f4bb78202d2b2a
serpient <littlegangwolf@gmail.com>
2018-11-26 20:39:44 -0800

    add projectUpdate mutation and teamResourceButton (wip)

--- src/components/ProjectCard/TeamResourceLinks.jsx
+++ src/components/ProjectCard/TeamResourceLinks.jsx
@@ -1,11 +1,53 @@
 import * as React from "react";
 import { Link } from "react-router-dom"
+import EditableTextField from '../utilities/EditableTextField';
+import { gql } from "apollo-boost";
 
-const TeamResourceLinks = ({ project }) => {
+// -- MUTATION -- //
+const projectUpdate = gql`
+mutation projectUpdate($project_id: ID!, $project_data: UserUpdateInput!) {
+  projectUpdate(project_id: $project_id, project_data: $project_data) {
+    id
+    links {
+      id
+      url
+      label
+    }
+  }
+}
+`;
+
+const TeamResourceLinks_DOM_ELEMENTS = [
+  {
+    divClassName: 'team-resource-link-container',
+    schemaKey: 'links',
+  }
+]
+const TeamResourceButton = ({ links }) => {
   return (
-    <React.Fragment>
-
-    </React.Fragment>
+    <Link to={links.url} target={'_blank'} className="team-resource-btn">
+      <img src={require('../../assets/Artboard 5.png')} className="team-resource-btn-icon" />
+      {links.label}
+    </Link>
+  )
+}
+const TeamResourceLinks = ({ project, editable }) => {
+  console.log(project);
+  return (
+    <div className="team-resource-link-container">
+      <div className="team-card-info--label">Team Resource Links</div>
+      <EditableTextField
+                    key={idx}
+                    mutation={projectUpdate}
+                    mutationName="projectUpdate"
+                    mutationInputName="links"
+                    fieldName={'links'}
+                    fieldData={value}
+                    hasPermission={editable}
+                    component={UserComponent}
+                    {...elem.editType}
+      />
+    </div>
   )
 }
 
