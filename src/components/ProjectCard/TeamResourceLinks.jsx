8087089e4b194dd55d2bbe87a283e95fb84871ca
serpient <littlegangwolf@gmail.com>
2018-11-27 01:44:40 -0800

    setup of team resource links

--- src/components/ProjectCard/TeamResourceLinks.jsx
+++ src/components/ProjectCard/TeamResourceLinks.jsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Link } from "react-router-dom"
 import EditableTextField from '../utilities/EditableTextField';
 import { gql } from "apollo-boost";
+import EditButton from '../common/EditButton';
 
 // -- MUTATION -- //
 const projectUpdate = gql`
@@ -17,38 +18,39 @@ mutation projectUpdate($project_id: ID!, $project_data: UserUpdateInput!) {
 }
 `;
 
-const TeamResourceLinks_DOM_ELEMENTS = [
-  {
-    divClassName: 'team-resource-link-container',
-    schemaKey: 'links',
-  }
-]
-const TeamResourceButton = ({ links }) => {
-  return (
-    <Link to={links.url} target={'_blank'} className="team-resource-btn">
-      <img src={require('../../assets/Artboard 5.png')} className="team-resource-btn-icon" />
-      {links.label}
-    </Link>
-  )
+const TeamResourceButtons = ({ links }) => {
+  if (!links) { return null };
+  return links.map((link, idx) => {
+    return (
+      <Link key={idx} to={link.url} target={'_blank'} className="team-resource-btn">
+        <img src={require('../../assets/Artboard 5.png')} className="team-resource-btn-icon" />
+        {link.label}
+      </Link>
+    )
+  })
 }
+
 const TeamResourceLinks = ({ project, editable }) => {
   console.log(project);
   return (
     <div className="team-resource-link-container">
       <div className="team-card-info--label">Team Resource Links</div>
       <EditableTextField
-                    key={idx}
-                    mutation={projectUpdate}
-                    mutationName="projectUpdate"
-                    mutationInputName="links"
-                    fieldName={'links'}
-                    fieldData={value}
-                    hasPermission={editable}
-                    component={UserComponent}
-                    {...elem.editType}
+        mutation={projectUpdate}
+        mutationName="projectUpdate"
+        mutationInputName="links"
+        fieldName={'links'}
+        fieldData={project.links}
+        hasPermission={true}
+        component={TeamResourceButtons}
       />
     </div>
   )
 }
 
 export default TeamResourceLinks;
+
+// team resource link renders a batch of editableTextFields/buttons
+// editing it will edit all inputs at once
+// there should be an input that allows you to add new resource links
+// setting an empty URL will remove that link option
