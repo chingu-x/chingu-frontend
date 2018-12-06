c8f17d0fc0d1ce33bc226dd6b7e537423c978f13
serpient <littlegangwolf@gmail.com>
2018-12-05 23:32:08 -0800

    resource link QA with popupMenu type new link button

--- src/components/ProjectCard/TeamResourceLinks.jsx
+++ src/components/ProjectCard/TeamResourceLinks.jsx
@@ -2,7 +2,9 @@ import * as React from "react";
 import { Link } from "react-router-dom"
 import EditableTextField from '../utilities/EditableTextField';
 import { gql } from "apollo-boost";
-import EditButton from '../common/EditButton';
+import PopupMenu from "../utilities/PopupMenu";
+import DynamicFormContainer from '../DynamicForm/components/DynamicFormContainer';
+import CreatableSelect from 'react-select/lib/Creatable';
 
 // -- MUTATION -- //
 const projectUpdate = gql`
@@ -18,6 +20,34 @@ mutation projectUpdate($project_id: ID!, $project_data: UserUpdateInput!) {
 }
 `;
 
+const NewResourceLinkQA = (value) => [
+  {
+    field_name: 'url',
+    text: 'URL Link',
+    input_type: 'text',
+  },
+  {
+    field_name: 'label',
+    text: 'Label',
+    input_type: value !== 'link' ? 'dropdown' : 'text',
+    options: value !== 'link'
+      && [
+      { text: 'AWS', value: 'aws' },
+      { text: 'Behance', value: 'behance' },
+      { text: 'Bitbucket', value: 'bitbucket' },
+      { text: 'Codepen', value: 'codepen' },
+      { text: 'Discord', value: 'discord' },
+      { text: 'Dropbox', value: 'dropbox' },
+      { text: 'Github', value: 'github' },
+      { text: 'Google Drive', value: 'google-drive' },
+      { text: 'Medium', value: 'medium' },
+      { text: 'Slack', value: 'slack' },
+      { text: 'Trello', value: 'trello' },
+      { text: 'Youtube', value: 'youtube' },
+      { text: 'Other', value: 'link' },
+    ]
+  }
+]
 const TeamResourceButtons = ({ links }) => {
   if (!links) { return null };
   return links.map((link, idx) => {
@@ -30,21 +60,40 @@ const TeamResourceButtons = ({ links }) => {
   })
 }
 
-const TeamResourceLinks = ({ project, editable }) => {
-  return (
-    <div className="team-resource-link-container">
-      <div className="team-card-info--label">Team Resource Links</div>
-      <EditableTextField
-        mutation={projectUpdate}
-        mutationName="projectUpdate"
-        mutationInputName="links"
-        fieldName={'links'}
-        fieldData={project.links}
-        hasPermission={true}
-        component={TeamResourceButtons}
-      />
-    </div>
-  )
+class TeamResourceLinks extends React.Component {
+  constructor(props) {
+    super(props);
+    state = {
+      value: 'link'
+    }
+  }
+
+  render() {
+    const { project } = this.props;
+    const { value } = this.state;
+    return (
+      <div className="team-resource-link-container">
+        <div className="team-card-info--label">Team Resource Links</div>
+        <PopupMenu className="new-team-resource-link-container">
+          <button className="add-new-link-btn">+</button>
+          <DynamicFormContainer
+            questions={NewResourceLinkQA(value)}
+            onSubmit={this.submitNewResourceLink}
+          />
+        </PopupMenu>
+        <EditableTextField
+          mutation={projectUpdate}
+          mutationName="projectUpdate"
+          mutationInputName="links"
+          fieldName={'links'}
+          fieldData={project.links}
+          hasPermission={true}
+          component={TeamResourceButtons}
+        />
+      </div>
+    )
+  }
+  
 }
 
 export default TeamResourceLinks;
