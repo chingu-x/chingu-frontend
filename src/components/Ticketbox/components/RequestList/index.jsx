b93cd702eb5544aa0eddcc4931fc66746c2f96c4
serpient <littlegangwolf@gmail.com>
2018-12-19 15:25:45 -0800

    add no ticket render case

--- src/components/Ticketbox/components/RequestList/index.jsx
+++ src/components/Ticketbox/components/RequestList/index.jsx
@@ -8,22 +8,32 @@ import HelpRequestCard from './HelpRequestCard';
 class RequestList extends React.Component {
   render() {
     const { data: { user }, loading, switchRenderedType } = this.props;
+    const imgFile = 'Tickets-small.png';
+    const imgSrc = require(`../../../../assets/${imgFile}`);
 
     let toRender;
     // todo: div wrapper to customize styling / positioning?
     if (loading) toRender = <Loader size="small" />;
     else {
       const { help_requests } = user;
-      if (!help_requests.length) return 'No help requests created';
-      toRender = help_requests.map(HelpRequestCard);
+      if (!help_requests.length) {
+        toRender = 'No tickets yet! Come back after you have submitted a Bug, Suggestion or Help Ticket.';
+      } else {
+        toRender = help_requests.map(HelpRequestCard);
+      }
     }
     // todo: if allow filter by status or request_type manage state of filter tabs
     // filter client side or move <Request /> query to be managed by RequestList
-    
+
     return (
       <div className="bug-suggestion-box">
-        {toRender}
-        <BackBtn type="center" path={""} switchRenderedType={switchRenderedType} />
+        <div className={`box-color color--tickets`}>
+          <img className="box-icon" alt="icon" src={imgSrc} />
+        </div>
+        <div className="ticketbox-form">
+          {toRender}
+          <BackBtn type="center" path={""} switchRenderedType={switchRenderedType} />
+        </div>
       </div>
     );
   }
