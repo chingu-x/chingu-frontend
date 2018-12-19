23a63dea60310b5d2e60effd43a0be871dba8ae8
The-Vampiire <vampiirecodes@gmail.com>
2018-12-18 23:11:41 -0500

    HelpRequest/questions: sort project list by tier and title

--- src/components/utilities/index.jsx
+++ src/components/utilities/index.jsx
@@ -1,7 +1,9 @@
-import introspectEnum from "./introspectEnum";
-import Request from "./Request";
+import introspectEnum from './introspectEnum';
+import Request from './Request';
+import sortByValue from './sortByValue';
 
 export {
   introspectEnum,
-  Request
+  Request,
+  sortByValue,
 };
