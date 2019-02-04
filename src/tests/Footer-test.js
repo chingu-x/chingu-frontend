45780f49443b6a051ac006c6475ee1da81744d7d
jdmedlock <jdmedlock@gmail.com>
2019-02-04 16:11:46 -0600

    Feature: Add prototype code for Footer component tests (IN-PROGRESS)
    Add prototype code for Footer component tests (IN-PROGRESS)
    - This is a work in-progress. Unfinished
    
    Resolves: N/a
    See also: #197

--- src/tests/Footer-test.js
+++ src/tests/Footer-test.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import { shallow } from 'enzyme';
 import Footer from '../components/Footer/Footer';
 
-test('Footer tbd', async () => {
-  // Render a Footer
-  const footer = shallow(<Footer />);
-  console.log('footer: ', footer.debug());
+describe('Footer page component', () => {
+  const footer = shallow(<Footer />); // TODO: move to setup
 
-  expect(footer.hasClass('connection-icon')); // Just for validating Jest setup
+  // Validate the structural integrity of the page
+  test('contains expected literals', () => {
+    // Render a Footer
+    console.log('footer: ', footer.debug());
+
+    const texts = footer.find('.footer-subtitle').map(node => node.text());
+    const expectedLiterals = ['Contact Us', 'Company'];
+    expect(texts).toEqual(expect.arrayContaining(expectedLiterals));
+  });
+
+  // Validate the functionality on the page
+  test('test navigatablity of react-router Link components', () => {
+    const links = footer.find('.footer-link').map(node => {
+      const linkPage = node.simulate("click");
+      console.log('linkePage: ', linkPage.debug());
+    });
+  });
 });
