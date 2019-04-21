df24986864fe62b301a6172b0a4166c6f3bfe1af
serpient <littlegangwolf@gmail.com>
2019-04-20 17:12:29 -0700

    add landing page copywriting

---
+++ src/components/Landing/copywriting.js
@@ -0,0 +1,115 @@
+const tagline = {
+  header: "Practice building projects to become job ready",
+  description: `Practice the technical and soft skills required in a professional software job through Chingu’s 10-week Cohort programs. 100% Online and Free.`
+};
+
+const chingu_value = {
+  minor: `What can Chingu do for me?`,
+  header: `Think you just need to know the right language to be a good programmer?`,
+  description: `Whether you are new to the industry or a seasoned developer, Chingu’s 10-week Cohorts are designed to help you level up.
+                  When Chingu places you in a team, you'll have opportunities to:
+                  `,
+  bullets: [
+    `Reinforce and strengthen your technical foundation`,
+    `Practice common “soft” skills (e.g. project management)`,
+    `Build projects in a team environment`
+  ]
+};
+
+const level_ups = {
+  base_skill: `Your Tech Stack`,
+  soft_skills: {
+    header: "Soft Skills",
+    skills: [
+      `Time Management`,
+      `Accountability & Reliability`,
+      `Handling Interpersonal Conflict`,
+      `Collaboration`,
+      `Clear, Respectful Communication`
+    ]
+  },
+  project: {
+    header: "Project Development",
+    skills: [
+      `Git / GitHub / Local Development`,
+      `Planning a Project`,
+      `Choosing the right technologies`,
+      `Project Setup to Deployment`
+    ]
+  },
+  teamwork: {
+    header: "Teamwork",
+    skills: [
+      `Team Git Workflows`,
+      `Familiarty with Other Peoples Code`,
+      `Developing Easily Maintainable Code`,
+      `Agile Project Management`,
+      `Mentoring Others`
+    ]
+  }
+};
+
+const min_requirements = {
+  minor: `Minimum Requirements`,
+  header: `Are you ready for Chingu?`,
+  description: `You’ll need, at minimum, intermediate HTML/CSS and basic JavaScript knowledge to qualify for Chingu Cohorts.
+                Know more than that? Chingu’s got you covered: our Cohorts have three different “tiers” separated by skill 
+                level so that you can work in the tier you feel most comfortable in. 
+                `,
+  more_info: `See more information about Tiers here.`
+};
+
+const tiers = [
+  {
+    header: `Tier 1 Requirements`,
+    description: `Intermediate grasp of HTML/CSS and a basic grasp of JavaScript (DOM manipulation and event loop).`
+  },
+  {
+    header: `Tier 2 Requirements`,
+    description: `Intermediate grasp of HTML, CSS and JavaScript. Familiarity with making API calls and interest in using a JavaScript library like React, Vue or Angular.`
+  },
+  {
+    header: `Tier 3 Requirements`,
+    description: `Advanced grasp of HTML, CSS and JavaScript. You may have some familiarity or interest in creating APIs and full stack projects. `
+  }
+];
+
+const cohort_schedule = {
+  header: `2019 Chingu Cohorts`,
+  spring: {
+    minor: `Spring`,
+    header: `Cohort 8`,
+    description: `Feb. - May`
+  },
+  summer: {
+    minor: `Summer`,
+    header: `Cohort 9`,
+    description: `May - Sept.`
+  },
+  fall: {
+    minor: `Fall`,
+    header: `Cohort 10`,
+    description: `Sept. - Nov.`
+  }
+};
+
+const cohort_breakdown = {
+  header: `Cohort Breakdown`,
+  pre_work: {
+    header: `Pre-Work`,
+    description: `11 Day`
+  },
+  voyage: {
+    header: `Voyage`,
+    description: `8 Week`
+  },
+  showcase: {
+    header: `Project Showcase`,
+    description: `1 Day`
+  }
+};
+
+const call_for_action = {
+  header: `Join Chingu's 10-Week Cohort`,
+  description: `Join our next Cohort to be a better developer.  All Online. All Free.`
+};
